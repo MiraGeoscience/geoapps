2efc1fcde9e32b4e213124c8eb64e87a83196db2
Dominique Fournier <dominiquef@mirageoscience.com>
2020-05-12 09:13:18 -0700

    Add option to export multiple data. Use pandas to export CSV with fields. Add 3D mesh creation to object_to_object interpolation

--- apps/functions/export.py
+++ apps/functions/export.py
@@ -7,9 +7,13 @@ from ipywidgets.widgets import VBox, HBox
 import discretize
 from .geoh5py.workspace import Workspace
 from .geoh5py.objects import Curve, BlockModel, Octree
-from .utils import export_curve_2_shapefile, export_grid_2_geotiff, octree_2_treemesh
+from .utils import (
+    export_curve_2_shapefile, export_grid_2_geotiff,
+    octree_2_treemesh, object_2_dataframe
+)
 from .selection import object_data_selection_widget
 from scipy.spatial import cKDTree
+from scipy.interpolate import LinearNDInterpolator
 
 
 def export_widget(h5file):
@@ -31,43 +35,44 @@ def export_widget(h5file):
             export.value = False  # Reset button
 
             entity = workspace.get_entity(objects.value)[0]
-            if entity.get_data(data.value):
-                data_values = entity.get_data(data.value)[0].values
+
+            if data.value:
+
+                data_values = {}
+
+                for key in data.value:
+                    if entity.get_data(key):
+                        data_values[key] = entity.get_data(key)[0].values
+                        data_values[key][
+                            (data_values[key] > 1e-38) * (data_values[key] < 2e-38)
+                        ] = no_data_value.value
             else:
-                data_values = []
+                data_values = {}
 
-            values = []
             if file_type.value == 'xyz':
-                if getattr(entity, 'vertices', None) is not None:
-                    values = entity.vertices
-                elif getattr(entity, 'centroids', None) is not None:
-                    values = entity.centroids
-
-                if len(values) > 0:
-                    if any(data_values):
-                        values = np.c_[values, data_values]
-
-                    np.savetxt(f"{out_dir + export_as.value}.xyz", values)
-                    print(f"Object saved to {out_dir + export_as.value}.xyz")
-                else:
-                    print("Could not find spatial data to export")
+                dataframe = object_2_dataframe(entity, fields=list(data_values.keys()))
+                dataframe.to_csv(f"{out_dir + export_as.value}" + ".csv", index=False)
 
             elif file_type.value == 'ESRI shapefile':
 
                 assert isinstance(entity, Curve), f"Only Curve objects are support for type {file_type.value}"
-                export_curve_2_shapefile(
-                    entity, attribute=data.value,
-                    file_name=out_dir + export_as.value,
-                    epsg=epsg_code.value
-                )
-                print(f"Object saved to {out_dir + export_as.value}.shp")
+
+                for key, item in data_values.items():
+                    export_curve_2_shapefile(
+                        entity, attribute=item,
+                        file_name=out_dir + export_as.value + "_" + key,
+                        epsg=epsg_code.value
+                    )
+                    print(f"Object saved to {out_dir + export_as.value}.shp")
 
             elif file_type.value == 'geotiff' and entity.get_data(data.value):
-                export_grid_2_geotiff(
-                    entity.get_data(data.value)[0],
-                    out_dir + export_as.value + ".tif", epsg_code.value,
-                )
-                print(f"Object saved to {out_dir + export_as.value}.tif")
+                for key, item in data_values.items():
+                    export_grid_2_geotiff(
+                        item,
+                        out_dir + export_as.value + "_" + key + ".tif",
+                        epsg_code.value,
+                    )
+                    print(f"Object saved to {out_dir + export_as.value}.tif")
 
             elif file_type.value == 'UBC format':
 
@@ -76,32 +81,46 @@ def export_widget(h5file):
                     mesh = octree_2_treemesh(entity)
 
                     models = {}
-                    if entity.get_data(data.value):
+                    for key, item in data_values.items():
                         ind = np.argsort(mesh._ubc_order)
                         data_obj = entity.get_data(data.value)[0]
-                        models[out_dir + data_obj.name + ".mod"] = data_obj.values[ind]
+                        models[out_dir + data_obj.name + "_" + key + ".mod"] = item[ind]
                     mesh.writeUBC(out_dir + export_as.value + ".msh", models=models)
 
                 else:
                     mesh = discretize.TensorMesh(
-                        [entity.u_cells, entity.v_cells, entity.z_cells]
+                        [
+                            entity.u_cells,
+                            entity.v_cells,
+                            entity.z_cells
+                        ]
                     )
-                    mesh.x0 = [entity.origin['x'], entity.origin['y'], entity.origin['z']]
+
+                    # Move the origin to the bottom SW corner
+                    mesh.x0 = [
+                        entity.origin['x'] + entity.u_cells[entity.u_cells < 0].sum(),
+                        entity.origin['y'] + entity.v_cells[entity.v_cells < 0].sum(),
+                        entity.origin['z'] + entity.z_cells[entity.z_cells < 0].sum()
+                    ]
 
                     mesh.writeUBC(out_dir + export_as.value + ".msh")
 
-                    if entity.get_data(data.value):
+                    if any(data_values):
+                        for key, item in data_values.items():
 
-                        data_obj = entity.get_data(data.value)[0]
+                            if mesh.x0[2] == entity.origin['z']:
+                                values = item.copy()
+                                values = values.reshape(
+                                    (mesh.nCz, mesh.nCx, mesh.nCy), order='F'
+                                )[::-1, :, :]
+                                values = values.reshape((-1, 1), order='F')
+                            else:
+                                values = item
 
-                        values = data_obj.values
-
-                        np.savetxt(out_dir + data_obj.name + ".mod", values)
-                        # model = np.reshape(values, (mesh.nCx, mesh.nCy, mesh.nCz), order='F')
-                        # model = model[:, :, ::-1]
-                        # # model = np.transpose(model, (1, 2, 0))
-                        #
-                        # mesh.writeModelUBC(out_dir + data_obj.name + ".mod", model.ravel())
+                            np.savetxt(
+                                out_dir + key + ".mod",
+                                values
+                            )
 
 
 
@@ -119,6 +138,12 @@ def export_widget(h5file):
         description='Export type',
     )
 
+    no_data_value = widgets.FloatText(
+        description="no-data-value",
+        value=-99999,
+        style={'description_width': 'initial'}
+    )
+
     epsg_code = widgets.Text(
         description="EPSG code:",
         indent=False,
@@ -129,7 +154,9 @@ def export_widget(h5file):
 
     file_type.observe(update_options)
 
-    objects, data = object_data_selection_widget(h5file)
+    objects, data = object_data_selection_widget(
+        h5file, select_multiple=True
+    )
 
     def update_name(_):
         export_as.value = objects.value.replace(":", "_")
@@ -151,10 +178,10 @@ def export_widget(h5file):
         indent=False,
         disabled=False
     )
-    return VBox([objects, data, type_widget, export_as, export])
+    return VBox([objects, HBox([data, no_data_value]), type_widget, export_as, export])
 
 
-def mesh_to_mesh_interpolation(h5file):
+def object_to_object_interpolation(h5file):
     def out_update(_):
 
         if out_mode.value == 'To Object:':
@@ -162,28 +189,32 @@ def mesh_to_mesh_interpolation(h5file):
             out_panel.children = [out_mode, mesh_dropdown]
 
         else:
-            out_panel.children = [out_mode, VBox([new_grid, ref_dropdown, core_cell_size, depth_core])]
+            out_panel.children = [
+                out_mode, new_grid_panel
+            ]
 
     def interpolate_call(_):
 
         if interpolate.value:
 
             mesh_in = workspace.get_entity(objects.value)[0]
-            model_in = workspace.get_entity(data.value)[0]
 
             if hasattr(mesh_in, "centroids"):
-                xyz = mesh_in.centroids
+                xyz = mesh_in.centroids.copy()
             elif hasattr(mesh_in, "vertices"):
-                xyz = mesh_in.vertices
+                xyz = mesh_in.vertices.copy()
+
+            # Create a tree for the input mesh
+            tree = cKDTree(xyz)
 
             if out_mode.value == 'To Object:':
 
                 mesh_out = workspace.get_entity(mesh_dropdown.value)[0]
 
                 if hasattr(mesh_out, "centroids"):
-                    xyz_out = mesh_out.centroids
+                    xyz_out = mesh_out.centroids.copy()
                 elif hasattr(mesh_out, "vertices"):
-                    xyz_out = mesh_out.vertices
+                    xyz_out = mesh_out.vertices.copy()
 
             else:
 
@@ -194,45 +225,140 @@ def mesh_to_mesh_interpolation(h5file):
                 elif hasattr(ref_in, "vertices"):
                     xyz_ref = ref_in.vertices
 
+
                 # Find extent of grid
                 h = np.asarray(core_cell_size.value.split(",")).astype(float).tolist()
 
-                xmin, xmax = xyz_ref[:, 0].min() - h[0] / 2., xyz_ref[:, 0].max() + h[0] / 2.
-                nodal_x = np.arange(xmin, xmax, h[0]) - xmin
+                pads = np.asarray(padding_distance.value.split(",")).astype(float).tolist()
 
-                ymin, ymax = xyz_ref[:, 1].min() - h[1] / 2., xyz_ref[:, 1].max() + h[1] / 2.
-                nodal_y = np.arange(ymin, ymax, h[1]) - ymin
-
-                zmin, zmax = xyz_ref[:, 2].max() - depth_core.value, xyz_ref[:, 2].max() + h[2] / 2.
-                nodal_z = np.arange(zmax, zmin, -h[2]) - zmax
+                # Use discretize to build a tensor mesh
+                mesh = discretize.utils.meshutils.mesh_builder_xyz(
+                    xyz_ref, h,
+                    padding_distance=[
+                        [pads[0], pads[1]],
+                        [pads[2], pads[3]],
+                        [pads[4], pads[5]]
+                    ],
+                    depth_core=depth_core.value,
+                    expansion_factor=expansion_fact.value,
+                )
 
                 mesh_out = BlockModel.create(
                     workspace,
-                    origin=[xmin, ymin, zmax],
-                    u_cell_delimiters=nodal_x,
-                    v_cell_delimiters=nodal_y,
-                    z_cell_delimiters=nodal_z,
+                    origin=[mesh.x0[0], mesh.x0[1], mesh.x0[2]],
+                    u_cell_delimiters=mesh.vectorNx - mesh.x0[0],
+                    v_cell_delimiters=mesh.vectorNy - mesh.x0[1],
+                    z_cell_delimiters=(mesh.vectorNz - mesh.x0[2]),
                     name=new_grid.value,
                 )
 
-                xyz_out = mesh_out.centroids
+                # Try to recenter on nearest
+                # Find nearest cells
+                rad, ind = tree.query(mesh_out.centroids)
+                ind_nn = np.argmin(rad)
 
-            tree = cKDTree(xyz)
-            # Find nearest cells
-            rad, ind = tree.query(xyz_out)
-            mesh_out.add_data({model_in.name + "_interp": {"values": model_in.values[ind]}})
+                d_xyz = mesh_out.centroids[ind_nn, :] - xyz[ind[ind_nn], :]
+
+                mesh_out.origin = np.r_[mesh_out.origin.tolist()] - d_xyz
+
+                xyz_out = mesh_out.centroids.copy()
+
+            values = {}
+            for field in data.value:
+                model_in = workspace.get_entity(field)[0]
+                values[field] = model_in.values.copy()
+                if space.value == 'Log':
+                    values[field] = np.log(values[field])
+
+            values_interp = {}
+            if method.value == 'Linear':
+
+                for key, value in values.items():
+                    F = LinearNDInterpolator(xyz, value)
+                    values_interp[key] = F(xyz_out)
+
+            elif method.value == 'Inverse Distance':
+
+                ang = np.deg2rad((450. - np.asarray(skew_angle.value)) % 360.)
+
+                R = np.r_[
+                    np.c_[np.cos(ang), np.sin(ang)],
+                    np.c_[-np.sin(ang), np.cos(ang)]
+                ]
+
+                center = np.mean(xyz, axis=0).reshape((3, 1))
+                xyz -= np.kron(center, np.ones(xyz.shape[0])).T
+                xyz[:, :2] = np.dot(R, xyz[:, :2].T).T
+                xyz[:, 1] *= skew_factor.value
+
+                tree = cKDTree(xyz)
+
+                xyz_out -= np.kron(center, np.ones(xyz_out.shape[0])).T
+                xyz_out[:, :2] = np.dot(R, xyz_out[:, :2].T).T
+                xyz_out[:, 1] *= skew_factor.value
+
+                # Find nearest cells
+                rad, ind = tree.query(xyz_out, 8)
+
+                for key, value in values.items():
+                    values_interp[key] = np.zeros(xyz_out.shape[0])
+                    weight = np.zeros(xyz_out.shape[0])
+
+                    for ii in range(8):
+                        values_interp[key] += value[ind[:, ii]]/(rad[:, ii] + 1e-1)
+                        weight += 1./(rad[:, ii] + 1e-1)
+
+                    values_interp[key] /= weight
+
+            else:
+                # Find nearest cells
+                for key, value in values.items():
+                    rad, ind = tree.query(xyz_out)
+                    values_interp[key] = value[ind]
+
+            for key in values_interp.keys():
+                if space.value == 'Log':
+                    values_interp[key] = np.exp(values_interp[key])
+
+                values_interp[key][np.isnan(values_interp[key])] = -99999
+
+                if method.value == 'Inverse Distance':
+                    values_interp[key][rad[:, 0] > max_distance.value] = -99999
+                else:
+                    values_interp[key][rad > max_distance.value] = -99999
+
+
+            if topography.value is not None:
+                topo = workspace.get_entity(topography.value)[0].vertices
+                xyz_out = mesh_out.centroids.copy()
+                F = LinearNDInterpolator(topo[:, :2], topo[:, 2])
+                z_interp = F(xyz_out[:, :2])
+
+                ind_nan = np.isnan(z_interp)
+                if any(ind_nan):
+                    tree = cKDTree(topo[:, :2])
+                    _, ind = tree.query(xyz_out[ind_nan, :2])
+                    z_interp[ind_nan] = topo[ind, 2]
+
+                for key in values_interp.keys():
+                    values_interp[key][xyz_out[:, 2] > z_interp] = -99999
+
+            for key in values_interp.keys():
+                mesh_out.add_data({key + "_interp": {"values": values_interp[key]}})
 
             interpolate.value = False
+
+            workspace.finalize()
 
     workspace = Workspace(h5file)
 
     names = list(workspace.list_objects_name.values())
 
-    objects, data = object_data_selection_widget(h5file)
+    objects, data = object_data_selection_widget(h5file, select_multiple=True)
 
     out_mode = widgets.RadioButtons(
-        options=['To Object:', 'Re-grid Tensor'],
-        value='Re-grid Tensor',
+        options=['To Object:', 'Create 3D Grid'],
+        value='To Object:',
         disabled=False
     )
 
@@ -270,6 +396,63 @@ def mesh_to_mesh_interpolation(h5file):
         style={'description_width': 'initial'}
     )
 
+    padding_distance = widgets.Text(
+        value="0, 0, 0, 0, 0, 0",
+        description='Pad Distance (W, E, N, S, D, U)',
+        disabled=False,
+        style={'description_width': 'initial'}
+    )
+
+    expansion_fact = widgets.FloatText(
+        value=1.05,
+        description='Expansion factor',
+        disabled=False,
+        style={'description_width': 'initial'}
+    )
+
+    space = widgets.RadioButtons(
+        options=['Linear', 'Log'],
+        value='Linear',
+        disabled=False
+    )
+
+    def method_update(_):
+
+        if method.value == 'Inverse Distance':
+            method_panel.children = [method, method_skew]
+        else:
+            method_panel.children = [method]
+
+    method = widgets.RadioButtons(
+        options=['Nearest', 'Linear', 'Inverse Distance'],
+        value='Nearest',
+        disabled=False
+    )
+
+    max_distance = widgets.FloatText(
+        value=0,
+        description='Maximum distance',
+        style={'description_width': 'initial'}
+    )
+
+    skew_angle = widgets.FloatText(
+        value=0,
+        description='Azimuth (d.dd)',
+        disabled=False,
+        style={'description_width': 'initial'}
+    )
+
+    skew_factor = widgets.FloatText(
+        value=1,
+        description='Factor',
+        disabled=False,
+        style={'description_width': 'initial'}
+    )
+
+    method_skew = VBox([widgets.Label('Skew interpolation'), skew_angle, skew_factor])
+    method_panel = HBox([method])
+
+    method.observe(method_update)
     interpolate = widgets.ToggleButton(
         value=False,
         description='Interpolate',
@@ -278,6 +461,30 @@ def mesh_to_mesh_interpolation(h5file):
 
     interpolate.observe(interpolate_call)
 
-    out_panel = VBox([out_mode, VBox([new_grid, ref_dropdown, core_cell_size, depth_core])])
+    topography = widgets.Dropdown(
+        options=[None] + names,
+        description="Cut with topo",
+        style={'description_width': 'initial'}
+    )
 
-    return VBox([objects, data, out_panel, interpolate])
+    new_grid_panel = VBox([
+            new_grid, ref_dropdown, core_cell_size, depth_core, padding_distance, expansion_fact
+        ])
+
+    out_panel = HBox([
+        out_mode, mesh_dropdown
+    ])
+
+    return VBox([
+        HBox([
+            VBox([widgets.Label("Input"), objects, data]),
+            VBox([widgets.Label("Output"), out_panel])
+            ]),
+        VBox([
+            widgets.Label("Interpolation Parameters"),
+            HBox([
+            VBox([widgets.Label("Space"), space]),
+            VBox([widgets.Label("Method"), method_panel])]), max_distance, topography
+            ]),
+        interpolate
+    ])
