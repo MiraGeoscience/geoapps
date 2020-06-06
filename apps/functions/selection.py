b5a8cbc9ce97648d6ef1dfb9434e339eee6179bd
Dominique Fournier <dominiquef@mirageoscience.com>
2020-06-06 13:21:12 -0700

    Clean up selection.py

--- apps/functions/selection.py
+++ apps/functions/selection.py
@@ -1,9 +1,12 @@
-from ipywidgets import Dropdown, VBox, HBox, interactive_output, SelectMultiple
-from .geoh5py.workspace import Workspace
+from geoh5py.workspace import Workspace
+from ipywidgets import Dropdown, HBox, SelectMultiple, VBox, interactive_output
+
 from .plotting import plot_plan_data_selection
 
 
-def object_data_selection_widget(h5file, plot=False, interactive=False, select_multiple=False):
+def object_data_selection_widget(
+    h5file, plot=False, interactive=False, select_multiple=False
+):
     """
 
     """
@@ -25,38 +28,31 @@ def object_data_selection_widget(h5file, plot=False, interactive=False, select_m
         obj = workspace.get_entity(objects.value)[0]
         data.options = obj.get_data_list()
 
-    objects = Dropdown(
-        options=names,
-        value=names[0],
-        description='Object:',
-    )
-
-    obj = workspace.get_entity(objects.value)[0]
+    objects = Dropdown(options=names, description="Object:",)
 
     if select_multiple:
-        data = SelectMultiple(
-            options=obj.get_data_list(),
-            value=[obj.get_data_list()[0]],
-            description='Data: ',
-        )
+        data = SelectMultiple(description="Data: ",)
     else:
-        data = Dropdown(
-            options=obj.get_data_list(),
-            value=obj.get_data_list()[0],
-            description='Data: ',
-        )
+        data = Dropdown(description="Data: ",)
 
-    objects.observe(updateList, names='value')
+    if len(names) > 0:
+        objects.value = names[0]
+        obj = workspace.get_entity(objects.value)[0]
+        data.options = obj.get_data_list()
+        if select_multiple:
+            data.value = [obj.get_data_list()[0]]
+        else:
+            data.options = obj.get_data_list()
+            data.value = obj.get_data_list()[0]
 
-    out = HBox([
-        VBox([objects, data]),
-        interactive_output(
-            listObjects, {
-                "obj_name": objects,
-                "data_name": data
-            }
-        )
-    ])
+    objects.observe(updateList, names="value")
+
+    out = HBox(
+        [
+            VBox([objects, data]),
+            interactive_output(listObjects, {"obj_name": objects, "data_name": data}),
+        ]
+    )
 
     if interactive:
         return out
