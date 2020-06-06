7ad2a4d86d30901a56b76cdf0bfbe83e5d94b2da
Dominique Fournier <dominiquef@mirageoscience.com>
2020-06-06 13:20:18 -0700

    Clean requirement and  __init__ to rely on geoh5py from pypi

--- apps/functions/__init__.py
+++ apps/functions/__init__.py
@@ -1,7 +1,1 @@
-from . import (
-    geoh5py, simpegEM1D, simpegPF
-)
-#
-# fem1d_inversion, tem1d_inversion, pf_inversion,
-# export, plotting, processing, selection, inversion,
-# utils
+from . import simpegEM1D, simpegPF
