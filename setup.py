64dee5b2eff9358965552486dae7f2a89fb98123
domfournier <dominiquef@mirageoscience.com>
2020-09-09 13:24:04 -0700

    Update setup.py
    Add docs for PyPI page

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
 
 from setuptools import find_packages
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 CLASSIFIERS = [
     "Development Status :: 2 - Pre-Alpha",
@@ -25,11 +27,14 @@ setup(
     author="Mira Geoscience",
     author_email="dominiquef@mirageoscience.com",
     description="Open-sourced Applications in Geoscience",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     keywords="earth sciences",
-    url="https://mirageoscience-apps.readthedocs.io/en/latest/",
-    download_url="https://github.com/MiraGeoscience/mirageoscience-apps.git",
+    url="https://geoapps.readthedocs.io/en/latest/index.html",
+    download_url="https://github.com/MiraGeoscience/geoapps.git",
     classifiers=CLASSIFIERS,
     platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
     license="MIT License",
     use_2to3=False,
 )
+
