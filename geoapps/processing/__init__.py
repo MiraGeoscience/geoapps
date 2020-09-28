835a410e3d4d97b697a7b0c8516aef21d9c2024b
Dominique Fournier <dominiquef@mirageoscience.com>
2020-09-28 12:01:22 -0700

    Move calcultor as class. Add app to list

--- geoapps/processing/__init__.py
+++ geoapps/processing/__init__.py
@@ -1,9 +1,7 @@
-from .processing import (
-    calculator,
-    coordinate_transformation_widget,
-)
+from .processing import coordinate_transformation_widget
 from .contours import ContourValues
 from .em_profiling import EMLineProfiler
 from .edge_detection import EdgeDetectionApp
 from .data_interpolation import DataInterpolation
 from .cdi_surface import CDICurve2Surface
+from .calculator import Calculator
