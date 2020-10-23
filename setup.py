fa1a695ccd48cf20788b0122c201cbe30af58fdb
Dominique Fournier <dominiquef@mirageoscience.com>
2020-10-22 22:49:11 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ CLASSIFIERS = [
 
 setup(
     name="geoapps",
-    version="0.2.0",
+    version="0.2.3",
     packages=find_packages(),
     install_requires=["numpy", "h5py", "scipy", "geoh5py"],
     author="Mira Geoscience",
