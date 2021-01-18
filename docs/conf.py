6300f8b0b163467d58d44b40944a6649abdbc260
Dominique Fournier <dominiquef@mirageoscience.com>
2021-01-18 15:27:23 -0800

    Bump version 0.2.9

--- docs/conf.py
+++ docs/conf.py
@@ -85,9 +85,9 @@ master_doc = "index"
 project = "geoapps"
 
 # The short X.Y version.
-version = "0.2.8"
+version = "0.2.9"
 # The full version, including alpha/beta/rc tags.
-release = "0.2.8"
+release = "0.2.9"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
