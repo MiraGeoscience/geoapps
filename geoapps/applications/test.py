ba64a8c53425b47d388025982fcd9f494c538439
Dominique Fournier <dominiquef@mirageoscience.com>
2021-01-18 14:30:09 -0800

    Test script

--- geoapps/applications/test.py
+++ geoapps/applications/test.py
@@ -2,11 +2,26 @@ from geoapps.processing import ContourValues
 
 from geoapps.plotting import ScatterPlots
 from geoapps.inversion import InversionApp
+from geoapps.processing import PeakFinder
+from dask.distributed import Client
 
-app = InversionApp()
-app.widget
+
+def run():
+    #
+    # app = InversionApp()
+    # app.widget
+
+    client = Client()
+    app = PeakFinder()
+    app.tem_checkbox.values = False
+    app.data.value = ["Sf[12]"]
+    app.widget
+
 
 # from geoapps.processing import ContourValues
 #
 # app = ContourValues()
 # app.widget
+
+if __name__ == "__main__":
+    run()
