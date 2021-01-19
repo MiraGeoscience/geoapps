cccca8985167e3603a94874a134af7d0d1615280
Dominique Fournier <dominiquef@mirageoscience.com>
2021-01-19 12:08:12 -0800

    Seperate install from codecov in workflow

--- geoapps/applications/test.py
+++ geoapps/applications/test.py
@@ -1,10 +1,17 @@
 from geoapps.processing import ContourValues
 
 from geoapps.plotting import ScatterPlots
-from geoapps.inversion import InversionApp
+from geoapps.processing import Clustering
 
-app = InversionApp()
+app = Clustering()
+app.h5file = (
+    r"C:\Users\dominiquef\Downloads\Geochem_DesurveyedAssay_Cluster_causesErrors.geoh5"
+)
+app.objects.value = "Renst_DH_Assays_DeSurveyedMiddle"
+obj, _ = app.get_selected_entities()
+app.data.value = [name for name in app.data.options if name in obj.get_data_list()]
 app.widget
+
 
 # from geoapps.processing import ContourValues
 #
