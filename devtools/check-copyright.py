d8c45df78b86a5e3a75bb9366a206c0ef69c964b
sebastienh <sebastienh@mirageoscience.com>
2021-02-22 20:52:43 -0500

    configure precommit
    for now, comment: flake8, mypy, pylint

---
+++ devtools/check-copyright.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+#  Copyright (c) 2021 Mira Geoscience Ltd.
+#
+# This file is part of geoapps.
+#
+# geoapps is distributed under the terms and conditions of the MIT License
+# (see LICENSE file at the root of this source code package).
+
+import re
+import sys
+from datetime import date
+
+if __name__ == "__main__":
+    current_year = date.today().year
+    copyright_re = re.compile(fr"\bcopyright \(c\) \b{current_year}\b", re.IGNORECASE)
+    files = sys.argv[1:]
+    max_lines = 10
+    report_files = []
+    for f in files:
+        with open(f, mode="r", encoding="utf-8") as file:
+            count = 0
+            has_dated_copyright = False
+            for line in file:
+                count += 1
+                if count >= max_lines:
+                    break
+                if re.search(copyright_re, line):
+                    has_dated_copyright = True
+                    break
+
+            if not has_dated_copyright:
+                report_files.append(f)
+
+    if len(report_files) > 0:
+        for f in report_files:
+            sys.stderr.write(f"{f}: No copyright or invalid year\n")
+        exit(1)
+
+# readonly CURRENT_YEAR=$(date +"%Y")
+
+# if ! grep -e "Copyright (c) .*$CURRENT_YEAR" $(head -10 $f) 2>&1 1>/dev/null; then
+#    echo "File '$f' has no copyright or an invalid year"
+#    exit 1
+# fi
