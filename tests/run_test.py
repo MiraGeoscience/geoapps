728759fc21a68fcc57fa119e9140feafb692b42a
Dominique Fournier <dominiquef@mirageoscience.com>
2021-03-03 07:53:05 -0800

    Update to run_test

--- tests/run_test.py
+++ tests/run_test.py
@@ -1,19 +1,26 @@
-import tempfile
-from pathlib import Path
+#  Copyright (c) 2021 Mira Geoscience Ltd.
+#
+#  This file is part of geoapps.
+#
+#  geoapps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+
+from shutil import copyfile
+
 import pytest
-from geoapps.processing import (
-    PeakFinder,
-    Calculator,
-    CoordinateTransformation,
-    ContourValues,
-    Surface2D,
-    Clustering,
-    DataInterpolation,
-    EdgeDetectionApp,
-)
+
 from geoapps.export import Export
 from geoapps.inversion import InversionApp
-from shutil import copyfile
+from geoapps.processing import (
+    Calculator,
+    Clustering,
+    ContourValues,
+    CoordinateTransformation,
+    DataInterpolation,
+    EdgeDetectionApp,
+    PeakFinder,
+    Surface2D,
+)
 
 project = "Project_work.geoh5"
 
@@ -22,60 +29,53 @@ def test_calculator():
     copyfile(r"..\assets\FlinFlon.geoh5", project)
     app = Calculator(h5file=project)
     app.trigger.click()
-    pass
 
 
 def test_coordinate_transformation():
     app = CoordinateTransformation(h5file=project)
     app.trigger.click()
-    pass
 
 
 def test_contour_values():
     app = ContourValues(h5file=project)
     app.trigger.click()
-    pass
 
 
 def test_create_surface():
     app = Surface2D(h5file=project)
     app.trigger.click()
-    pass
 
 
 def test_clustering():
     app = Clustering(h5file=project)
     app.trigger.click()
-    pass
 
 
 def test_data_interpolation():
     app = DataInterpolation(h5file=project)
     app.trigger.click()
-    pass
 
 
 def test_edge_detection():
     app = EdgeDetectionApp(h5file=project)
     app.trigger.click()
-    pass
 
 
 def test_export():
     app = Export(h5file=project)
     app.trigger.click()
-    pass
 
 
 def test_inversion():
-    app = InversionApp(h5file=project, inversion_parameters={"max_iterations": 1},)
+    app = InversionApp(
+        h5file=project,
+        inversion_parameters={"max_iterations": 1},
+    )
     app.write.value = True
     app.run.value = True
-    pass
 
 
 def test_peak_finder():
     app = PeakFinder(h5file=project)
     app.run_all.click()
     app.trigger.click()
-    pass
