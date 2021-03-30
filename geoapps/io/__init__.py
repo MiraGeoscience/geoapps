398d44f2c86a2358e0452049912da6ed197c79df
benk-mira <benjamink@mirageoscience.com>
2021-03-30 16:15:34 -0700

    Prelim design for Params class.  Implemented inversion_type and inversion_style parameters only so far.

--- geoapps/io/__init__.py
+++ geoapps/io/__init__.py
@@ -5,4 +5,4 @@
 #  geoapps is distributed under the terms and conditions of the MIT License
 #  (see LICENSE file at the root of this source code package).
 
-from .driver import InputFile
+from .driver import InputFile, Params
--- geoapps/io/driver.py
+++ geoapps/io/driver.py
@@ -8,32 +8,101 @@
 import json
 import os
 
+from .constants import valid_parameter_values, valid_parameters
+
 
 class InputFile:
-    def __init__(self, filename):
-        self._accepted_file_extensions = ["json"]
+
+    _valid_parameters = valid_parameters
+    _required_parameters = ["inversion_type", "core_cell_size"]
+    _valid_extensions = ["json"]
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.workpath = self.create_work_path()
         self.data = None
-        self.filename = filename
+        self.itype = None
+        self.load()
 
     @property
-    def filename(self):
-        return self._filename
+    def filepath(self):
+        return self._filepath
 
-    @filename.setter
-    def filename(self, f):
-        if f.split(".")[-1] not in self._accepted_file_extensions:
+    @filepath.setter
+    def filepath(self, f):
+        if f.split(".")[-1] not in self._valid_extensions:
             raise OSError("Input file must have '.json' extension.")
         else:
-            self._filename = f
+            self._filepath = f
 
     def create_work_path(self):
         """ Creates absolute path to input file. """
         dsep = os.path.sep
-        workDir = os.path.dirname(os.path.abspath(self.filename)) + dsep
+        workpath = os.path.dirname(os.path.abspath(self.filepath)) + dsep
 
-        return workDir
+        return workpath
 
     def load(self):
         """ Loads input file contents to dictionary. """
-        with open(self.filename) as f:
+        with open(self.filepath) as f:
             self.data = json.load(f)
+            self._validate_required_parameters()
+            self._validate_parameters()
+            self.itype = self.data["inversion_type"]
+
+    def _validate_parameters(self):
+        """ Ensures that all the input files keys are accepted strings."""
+        for k in self.data.keys():
+            if k not in self._valid_parameters:
+                raise ValueError(f"Encountered an invalid input parameter: {k}.")
+
+    def _validate_required_parameters(self):
+        """ Ensures that all required input file keys are present."""
+        for param in self._required_parameters:
+            if param not in self.data.keys():
+                raise ValueError(f"Missing a required parameter: {param}.")
+
+
+class Params:
+
+    _valid_parameter_values = valid_parameter_values
+
+    def __init__(self, inputfile):
+        self.itype = inputfile.itype
+        self.workpath = inputfile.workpath
+        self._inversion_style = "voxel"
+        self._load_params_from_input_file(inputfile)
+
+    @property
+    def itype(self):
+        return self._itype
+
+    @property
+    def inversion_style(self):
+        return self._inversion_style
+
+    @itype.setter
+    def itype(self, val):
+        self._validate_parameter_value("inversion_type", val)
+        self._itype = val
+
+    @inversion_style.setter
+    def inversion_style(self, val):
+        self._validate_parameter_value("inversion_style", val)
+        self._inversion_style = val
+
+    def _load_params_from_input_file(self, inputfile):
+        """ Overrides default parameter values input file values. """
+        if "inversion_style" in inputfile.data.keys():
+            self.inversion_style = inputfile.data["inversion_style"]
+
+    def _validate_parameter_value(self, param, val):
+        """ Validates parameter values against accepted values. """
+        vpvals = self._valid_parameter_values[param]
+        nvals = len(vpvals)
+        if nvals > 1:
+            msg = f"Invalid {param} value. Must be one of: {vpvals}"
+        else:
+            msg = f"Invalid {param} value.  Must be: {vpvals}"
+        if val not in vpvals:
+            raise ValueError(msg)
