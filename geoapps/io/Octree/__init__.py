4173a0d631eb097060c636bbad6b6075e00c034c
Dominique Fournier <dominiquef@mirageoscience.com>
2021-06-01 09:48:06 -0700

    Add core modules in io/octree

---
+++ geoapps/io/Octree/__init__.py
@@ -0,0 +1,8 @@
+#  Copyright (c) 2021 Mira Geoscience Ltd.
+#
+#  This file is part of geoapps.
+#
+#  geoapps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+
+from .params import OctreeParams
