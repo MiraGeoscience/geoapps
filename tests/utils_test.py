a8abb38ffc446d6d3595b3c6245f2de4daad996e
benk-mira <benjamink@mirageoscience.com>
2021-06-09 13:12:30 -0700

    difference less than threshold instead of equality

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -170,4 +170,4 @@ def test_octree_2_treemesh():
             setattr(omesh, attr, -1 * getattr(omesh, attr))
             omesh.origin["xyz"["uvw".find(axis)]] = 40
         tmesh = octree_2_treemesh(omesh)
-        assert np.all(tmesh.cell_centers == mesh.cell_centers)
+        assert np.all((tmesh.cell_centers - mesh.cell_centers) < 1e-16)
