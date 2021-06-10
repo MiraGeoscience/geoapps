1c52606c9d02900b2328ddd2bc08821e2716205f
Dominique Fournier <dominiquef@mirageoscience.com>
2021-06-10 16:14:05 -0700

    Fix bound_tests ?

--- tests/params_test.py
+++ tests/params_test.py
@@ -683,14 +683,14 @@ def test_validate_lower_bound(tmp_path):
     param = "lower_bound"
     newval = -1000
     param_test_generator(tmp_path, param, newval)
-    catch_invalid_generator(tmp_path, param, "test", "type")
+    catch_invalid_generator(tmp_path, param, {}, "type")
 
 
 def test_validate_upper_bound(tmp_path):
     param = "upper_bound"
     newval = 1000
     param_test_generator(tmp_path, param, newval)
-    catch_invalid_generator(tmp_path, param, "test", "type")
+    catch_invalid_generator(tmp_path, param, {}, "type")
 
 
 def test_validate_parallelized(tmp_path):
