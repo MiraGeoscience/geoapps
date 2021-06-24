484fafd39f002cc4c84e066650d9bcf72f4ce356
benk-mira <benjamink@mirageoscience.com>
2021-06-24 13:12:12 -0700

    removing import for mt_inversion.py - failing pymatsolver.Pardiso import.

--- geoapps/drivers/__init__.py
+++ geoapps/drivers/__init__.py
@@ -5,4 +5,4 @@
 #  geoapps is distributed under the terms and conditions of the MIT License
 #  (see LICENSE file at the root of this source code package).
 
-from . import components, grav_inversion, mt_inversion, mvi_inversion, pf_inversion
+from . import components, grav_inversion, mvi_inversion, pf_inversion
