Fixed rotation bugs

geoapps/drivers/components/locations.py
@@ -48,7 +48,7 @@ class InversionLocations:
         mesh = workspace.get_entity(params.mesh)[0]
         if mesh.rotation is not None:
             self.origin = np.asarray(mesh.origin.tolist())
-            self.angle = mesh.rotation[0]
+            self.angle = -mesh.rotation[0]
             self.is_rotated = True
 
     @property