2514390576be24bd1072c13c71a2e4b8e7e6c175
sebastienh <sebastienh@mirageoscience.com>
2021-07-19 14:26:44 -0400

    fix configuration of the dev environment

--- devtools/setup-dev.bat
+++ devtools/setup-dev.bat
@@ -1,3 +1,4 @@
 @echo off
 set project_dir=%~dp0..\
-conda env create -p %project_dir%\.conda-env python=3.9 --file %project_dir%\environment.yml --file %project_dir%\environment-dev.yml
+conda env create -p %project_dir%\.conda-env python=3.9 --file %project_dir%\environment.yml
+conda activate %project_dir%\.conda-env && conda install pytest pylint
