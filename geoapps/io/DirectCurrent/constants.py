84c813a67c871967551818f7b440d2a3948ac497
Dominique Fournier <dominiquef@mirageoscience.com>
2021-08-20 10:46:16 -0700

    Add DirectCurrent io

---
+++ geoapps/io/DirectCurrent/constants.py
@@ -0,0 +1,188 @@
+#  Copyright (c) 2021 Mira Geoscience Ltd.
+#
+#  This file is part of geoapps.
+#
+#  geoapps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+
+from uuid import UUID
+
+import numpy as np
+
+from geoapps.io.Inversion.constants import default_ui_json as base_default_ui_json
+from geoapps.io.Inversion.constants import (
+    required_parameters as base_required_parameters,
+)
+from geoapps.io.Inversion.constants import validations as base_validations
+
+inversion_defaults = {
+    "inversion_type": "direct_current",
+    "forward_only": False,
+    "topography_object": None,
+    "topography": None,
+    "data_object": None,
+    "data_channel": None,
+    "data_uncertainty": 0.0,
+    "starting_model_object": None,
+    "starting_model": 0.0,
+    "tile_spatial": 1,
+    "z_from_topo": True,
+    "receivers_radar_drape": None,
+    "receivers_offset_x": 0,
+    "receivers_offset_y": 0,
+    "receivers_offset_z": 0,
+    "gps_receivers_offset": None,
+    "ignore_values": None,
+    "resolution": 50.0,
+    "detrend_data": False,
+    "detrend_order": 0,
+    "detrend_type": "all",
+    "max_chunk_size": 128,
+    "chunk_by_rows": False,
+    "output_tile_files": False,
+    "mesh": None,
+    "mesh_from_params": False,
+    "u_cell_size": 25.0,
+    "v_cell_size": 25.0,
+    "w_cell_size": 25.0,
+    "octree_levels_topo": [0, 0, 0, 2],
+    "octree_levels_obs": [5, 5, 5, 5],
+    "depth_core": 500.0,
+    "max_distance": np.inf,
+    "horizontal_padding": 1000.0,
+    "vertical_padding": 1000.0,
+    "window_center_x": 0.0,
+    "window_center_y": 0.0,
+    "window_width": 0.0,
+    "window_height": 0.0,
+    "inversion_style": "voxel",
+    "chi_factor": 1.0,
+    "max_iterations": 25,
+    "max_cg_iterations": 30,
+    "max_global_iterations": 100,
+    "initial_beta_ratio": 1e1,
+    "initial_beta": 0.0,
+    "tol_cg": 1e-16,
+    "alpha_s": 1.0,
+    "alpha_x": 1.0,
+    "alpha_y": 1.0,
+    "alpha_z": 1.0,
+    "smallness_norm": 2.0,
+    "x_norm": 2.0,
+    "y_norm": 2.0,
+    "z_norm": 2.0,
+    "reference_model_object": None,
+    "reference_model": None,
+    "gradient_type": "total",
+    "lower_bound_object": None,
+    "lower_bound": -1,
+    "upper_bound_object": None,
+    "upper_bound": 1,
+    "parallelized": True,
+    "n_cpu": None,
+    "max_ram": 2,
+    "workspace": None,
+    "out_group": "DirectCurrentInversion",
+    "no_data_value": None,
+    "monitoring_directory": None,
+    "geoh5": None,
+    "run_command": "geoapps.drivers.direct_current_inversion",
+    "run_command_boolean": False,
+    "conda_environment": "geoapps",
+}
+forward_defaults = {
+    "inversion_type": "direct_current",
+    "forward_only": True,
+    "topography_object": None,
+    "topography": None,
+    "data_object": None,
+    "starting_model_object": None,
+    "starting_model": None,
+    "tile_spatial": 1,
+    "z_from_topo": True,
+    "receivers_radar_drape": None,
+    "receivers_offset_x": 0,
+    "receivers_offset_y": 0,
+    "receivers_offset_z": 0,
+    "gps_receivers_offset": None,
+    "resolution": 50.0,
+    "max_chunk_size": 128,
+    "chunk_by_rows": False,
+    "mesh": None,
+    "mesh_from_params": False,
+    "u_cell_size": 25.0,
+    "v_cell_size": 25.0,
+    "w_cell_size": 25.0,
+    "octree_levels_topo": [0, 0, 0, 2],
+    "octree_levels_obs": [5, 5, 5, 5],
+    "depth_core": 500.0,
+    "max_distance": np.inf,
+    "horizontal_padding": 1000.0,
+    "vertical_padding": 1000.0,
+    "window_center_x": 0.0,
+    "window_center_y": 0.0,
+    "window_width": 0.0,
+    "window_height": 0.0,
+    "parallelized": True,
+    "n_cpu": None,
+    "max_ram": 2,
+    "workspace": None,
+    "out_group": "DirectCurrentForward",
+    "monitoring_directory": None,
+    "geoh5": None,
+    "run_command": "geoapps.drivers.direct_current_inversion",
+    "run_command_boolean": False,
+    "conda_environment": "geoapps",
+}
+default_ui_json = {
+    "inversion_type": "direct_current",
+    "data_object": {
+        "main": True,
+        "group": "Data",
+        "label": "Object",
+        "meshType": [
+            "{9b08bb5a-300c-48fe-9007-d206f971ea92}",
+        ],
+        "value": None,
+    },
+    "data_channel": {
+        "association": "Cell",
+        "dataType": "Float",
+        "default": None,
+        "group": "Data",
+        "main": True,
+        "label": "Potential channel",
+        "parent": "data_object",
+        "value": None,
+    },
+    "data_uncertainty": {
+        "association": "Cell",
+        "dataType": "Float",
+        "default": 0.0,
+        "group": "Data",
+        "main": True,
+        "isValue": True,
+        "label": "Potential uncertainty",
+        "parent": "data_object",
+        "property": None,
+        "value": 0.0,
+    },
+    "out_group": {"label": "Results group name", "value": "DirectCurrent"},
+}
+default_ui_json.update(base_default_ui_json)
+default_ui_json = {k: default_ui_json[k] for k in inversion_defaults}
+required_parameters = ["inversion_type"]
+required_parameters += base_required_parameters
+validations = {
+    "inversion_type": {
+        "types": [str],
+        "values": ["direct_current"],
+    },
+    "data_channel": {
+        "types": [str, UUID],
+        "reqs": [("data_object")],
+    },
+    "data_uncertainty": {"types": [str, int, float]},
+}
+
+validations.update(base_validations)
---
+++ geoapps/io/DirectCurrent/params.py
@@ -0,0 +1,97 @@
+#  Copyright (c) 2021 Mira Geoscience Ltd.
+#
+#  This file is part of geoapps.
+#
+#  geoapps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+
+from __future__ import annotations
+
+from uuid import UUID
+
+from geoapps.io.Inversion import InversionParams
+
+from ..validators import InputValidator
+from .constants import (
+    default_ui_json,
+    forward_defaults,
+    inversion_defaults,
+    required_parameters,
+    validations,
+)
+
+
+class DirectCurrentParams(InversionParams):
+
+    _required_parameters = required_parameters
+    _validations = validations
+
+    def __init__(self, forward=False, **kwargs):
+
+        self.validator: InputValidator = InputValidator(
+            required_parameters, validations
+        )
+        self.inversion_type = "direct_current"
+        self.data_channel = None
+        self.data_uncertainty = None
+        self.out_group = None
+        self.defaults = forward_defaults if forward else inversion_defaults
+        self.default_ui_json = {k: default_ui_json[k] for k in self.defaults}
+        self.param_names = list(self.default_ui_json.keys())
+
+        for k, v in self.default_ui_json.items():
+            if isinstance(v, dict):
+                field = "value"
+                if "isValue" in v.keys():
+                    if not v["isValue"]:
+                        field = "property"
+                self.default_ui_json[k][field] = self.defaults[k]
+            else:
+                self.default_ui_json[k] = self.defaults[k]
+
+        super().__init__(**kwargs)
+
+    @property
+    def inversion_type(self):
+        return self._inversion_type
+
+    @inversion_type.setter
+    def inversion_type(self, val):
+        if val is None:
+            self._inversion_type = val
+            return
+        p = "inversion_type"
+        self.validator.validate(
+            p, val, self.validations[p], self.workspace, self.associations
+        )
+        self._inversion_type = val
+
+    @property
+    def data_channel(self):
+        return self._data_channel
+
+    @data_channel.setter
+    def data_channel(self, val):
+        if val is None:
+            self._data_channel = val
+            return
+        p = "data_channel"
+        self.validator.validate(
+            p, val, self.validations[p], self.workspace, self.associations
+        )
+        self._data_channel = UUID(val) if isinstance(val, str) else val
+
+    @property
+    def data_uncertainty(self):
+        return self._data_uncertainty
+
+    @data_uncertainty.setter
+    def data_uncertainty(self, val):
+        if val is None:
+            self._data_uncertainty = val
+            return
+        p = "data_uncertainty"
+        self.validator.validate(
+            p, val, self.validations[p], self.workspace, self.associations
+        )
+        self._data_uncertainty = UUID(val) if isinstance(val, str) else val
