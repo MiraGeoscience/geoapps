30a6aa196443b5404bcf6244bae2fc7a974fca66
Dominique Fournier <dominiquef@mirageoscience.com>
2021-09-21 09:04:07 -0700

    one more try

--- tests/run_tests/apps_run_test.py
+++ tests/run_tests/apps_run_test.py
@@ -73,15 +73,7 @@ def test_export():
     app.trigger.click()
 
 
-def test_inversion(tmp_path):
-    test_ws_path = "test.geoh5"
-    geotest = Geoh5Tester(workspace, tmp_path, test_ws_path)
-    geotest.copy_entity(UUID("{ab3c2083-6ea8-4d31-9230-7aad3ec09525}"))
-    geotest.copy_entity(UUID("{538a7eb1-2218-4bec-98cc-0a759aa0ef4f}"))
-    geotest.copy_entity(UUID("{a603a762-f6cb-4b21-afda-3160e725bf7d}"))
-    geotest.copy_entity(UUID("{6de9177a-8277-4e17-b76c-2b8b05dcf23c}"))
-    geotest.copy_entity(UUID("{44822654-b6ae-45b0-8886-2d845f80f422}"))
-
+def test_inversion():
     params = {
         "w_cell_size": 60,
         "z_from_topo": False,
@@ -97,7 +89,7 @@ def test_inversion(tmp_path):
 
     side_effects = {"starting_inclination": 35, "detrend_type": "all"}
 
-    app = InversionApp(h5file=geotest.ws.h5file, plot_result=False, **params)
+    app = InversionApp(h5file=project, plot_result=False, **params)
 
     for param, value in changes.items():
         if isinstance(getattr(app, param), Widget):
