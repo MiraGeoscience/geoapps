fa420c73563f5fdc516aa1335dcafc2fdc6f1773
benk-mira <benjamink@mirageoscience.com>
2021-10-13 09:41:41 -0700

    trying to transform behaviour with tiles and channels.

--- geoapps/drivers/components/factories/directives_factory.py
+++ geoapps/drivers/components/factories/directives_factory.py
@@ -143,16 +143,14 @@ class SaveIterationGeoh5Factory(SimPEGFactory):
 
         if object_type == "data":
 
-            channels = inversion_object.observed.keys()
+            channels = list(inversion_object.observed.keys())
             kwargs["channels"] = channels
             kwargs["attribute_type"] = "predicted"
             kwargs["save_objective_function"] = True
-            kwargs["transforms"] = [
-                np.tile(
-                    [inversion_object.normalizations[c] for c in channels],
-                    (len(inversion_object.observed), 1),
-                )
-            ]
+            kwargs["transforms"] = np.tile(
+                [inversion_object.normalizations[c] for c in channels],
+                inversion_object.locations.shape[0],
+            )
 
             if self.factory_type == "direct current":
 
