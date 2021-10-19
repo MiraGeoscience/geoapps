c05293c6ed9b9a69cab055583ae4a2b36af3b541
sebastienh <sebastienh@mirageoscience.com>
2021-10-19 18:29:35 -0400

    GEOPY-351: install geoapps within the conda env in editable mode

--- Start_applications.bat
+++ Start_applications.bat
@@ -2,8 +2,6 @@ set CONDA_EXE=conda
 :: if Conda executable is not in the PATH, uncomment and set the executable location below
 ::set CONDA_EXE=%USERPROFILE%\AppData\Local\Continuum\anaconda3\Library\bin\conda.bat
 
-set THIS_DIR=%~dp0
-set PYTHONPATH=%THIS_DIR%;%PYTHONPATH%
-cd %THIS_DIR%\geoapps\applications
+cd %~dp0\geoapps\applications
 call %CONDA_EXE% activate geoapps && jupyter notebook Index.ipynb
 cmd /k
