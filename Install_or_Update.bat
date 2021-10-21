48f0cd6de573277f2582167ba18878deaa8a8eb7
sebastienh <sebastienh@mirageoscience.com>
2021-10-20 22:09:17 -0400

    script to find conda.bat

--- Install_or_Update.bat
+++ Install_or_Update.bat
@@ -1,11 +1,15 @@
-set CONDA_EXE=conda
-:: if Conda executable is not in the PATH, uncomment and set the executable location below
-::set CONDA_EXE=%USERPROFILE%\AppData\Local\Continuum\anaconda3\Library\bin\conda.bat
+@echo off
+setlocal EnableDelayedExpansion
+
+call %~dp0get_conda_exec.bat
+if !errorlevel! neq 0 (
+  exit /B !errorlevel!
+)
 
 cd %~dp0
-call %CONDA_EXE% remove --name geoapps --all --yes
-call %CONDA_EXE% env update --file environment.yml --prune
-call %CONDA_EXE% env config vars set KMP_WARNINGS=0 --name geoapps
-call %CONDA_EXE% activate geoapps && python -m pip install -e .
+call !MY_CONDA_EXE! remove --name geoapps --all --yes
+call !MY_CONDA_EXE! env update --file environment.yml --prune
+call !MY_CONDA_EXE! env config vars set KMP_WARNINGS=0 --name geoapps
+call !MY_CONDA_EXE! activate geoapps && python -m pip install -e .
 pause
 cmd /k
