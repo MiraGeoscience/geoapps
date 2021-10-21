19e69e9904ce4324353f34741a0a5ac034c0befb
sebastienh <sebastienh@mirageoscience.com>
2021-10-21 17:17:36 -0400

    GEOPY-358: use newly introcude get_conda_exec

--- devtools/setup-dev.bat
+++ devtools/setup-dev.bat
@@ -1,5 +1,14 @@
 @echo off
-set project_dir=%~dp0..\
+setlocal EnableDelayedExpansion
+
+set project_dir=%~dp0..
+call %project_dir%\get_conda_exec.bat
+if !errorlevel! neq 0 (
+  exit /B !errorlevel!
+)
+
 set env_path=%project_dir%\.conda-env
-conda env create -p %env_path% python=3.9 --file %project_dir%\environment.yml ^
- & conda install -y -p %env_path% --file %project_dir%\dev-extra-requirements.txt
+call !MY_CONDA_EXE! env create -p %env_path% python=3.9 --file %project_dir%\environment.yml
+call !MY_CONDA_EXE! install -y -p %env_path% --file %project_dir%\dev-extra-requirements.txt
+pause
+cmd /k
