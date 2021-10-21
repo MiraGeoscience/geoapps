034a5fe1c85c3fb904f2719acd8e2dcd6c8dc0ac
sebastienh <sebastienh@mirageoscience.com>
2021-10-21 17:27:23 -0400

    GEOPY-358: in dev environment, reinstall local geoh5py as editable
    the geoh5py clone is expected to a sibling folder of geoapps

--- devtools/setup-dev.bat
+++ devtools/setup-dev.bat
@@ -10,5 +10,6 @@ if !errorlevel! neq 0 (
 set env_path=%project_dir%\.conda-env
 call !MY_CONDA_EXE! env create -p %env_path% python=3.9 --file %project_dir%\environment.yml
 call !MY_CONDA_EXE! install -y -p %env_path% --file %project_dir%\dev-extra-requirements.txt
+call !MY_CONDA_EXE! activate %env_path% && pip install --upgrade --force-reinstall -e %project_dir%\..\geoh5py --no-deps
 pause
 cmd /k
