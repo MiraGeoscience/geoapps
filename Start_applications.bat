2fbf83b54e6ffc000cd56157637bd679f848ce87
sebastienh <sebastienh@mirageoscience.com>
2021-10-20 22:09:17 -0400

    script to find conda.bat

--- Start_applications.bat
+++ Start_applications.bat
@@ -1,7 +1,11 @@
-set CONDA_EXE=conda
-:: if Conda executable is not in the PATH, uncomment and set the executable location below
-::set CONDA_EXE=%USERPROFILE%\AppData\Local\Continuum\anaconda3\Library\bin\conda.bat
+@echo off
+setlocal EnableDelayedExpansion
 
-cd %~dp0\geoapps\applications
-call %CONDA_EXE% activate geoapps && jupyter notebook Index.ipynb
+call %~dp0get_conda_exec.bat
+if !errorlevel! neq 0 (
+  exit /B !errorlevel!
+)
+
+cd %~dp0geoapps\applications
+call !MY_CONDA_EXE! activate geoapps && jupyter notebook Index.ipynb
 cmd /k
