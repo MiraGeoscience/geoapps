83f49f1208d104185f9cb68fc92f701ee776409f
sebastienh <sebastienh@mirageoscience.com>
2021-10-21 14:52:15 -0400

    GEOPY-351: install geoapps with --no-deps
    Dependencies are already in the conda env.
    Also avoids installation failure in some instances.

--- Install_or_Update.bat
+++ Install_or_Update.bat
@@ -9,6 +9,6 @@ if !errorlevel! neq 0 (
 cd %~dp0
 call !MY_CONDA_EXE! remove --name geoapps --all --yes
 call !MY_CONDA_EXE! env update --file environment.yml --prune
-call !MY_CONDA_EXE! activate geoapps && python -m pip install -e .
+call !MY_CONDA_EXE! activate geoapps && python -m pip install -e . --no-deps
 pause
 cmd /k
