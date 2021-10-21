34edada57ea47f3a3704f21a1fad22988acd998b
sebastienh <sebastienh@mirageoscience.com>
2021-10-21 12:01:16 -0400

    GEOPY-351: fix when double-clicking on bat file
    Must make sure to cd to the folder.
    Was working when calling from command line.

--- Install_or_Update.bat
+++ Install_or_Update.bat
@@ -2,6 +2,7 @@ set CONDA_EXE=conda
 :: if Conda executable is not in the PATH, uncomment and set the executable location below
 ::set CONDA_EXE=%USERPROFILE%\AppData\Local\Continuum\anaconda3\Library\bin\conda.bat
 
+cd %~dp0
 call %CONDA_EXE% remove --name geoapps --all --yes
 call %CONDA_EXE% env update --file environment.yml --prune
 call %CONDA_EXE% env config vars set KMP_WARNINGS=0 --name geoapps
