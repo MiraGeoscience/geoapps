15d38b1e79f589c382b9eb42c83685934223b6ff
sebastienh <sebastienh@mirageoscience.com>
2021-10-21 13:51:15 -0400

    use environment.yml to set variables

--- Install_or_Update.bat
+++ Install_or_Update.bat
@@ -9,7 +9,6 @@ if !errorlevel! neq 0 (
 cd %~dp0
 call !MY_CONDA_EXE! remove --name geoapps --all --yes
 call !MY_CONDA_EXE! env update --file environment.yml --prune
-call !MY_CONDA_EXE! env config vars set KMP_WARNINGS=0 --name geoapps
 call !MY_CONDA_EXE! activate geoapps && python -m pip install -e .
 pause
 cmd /k
