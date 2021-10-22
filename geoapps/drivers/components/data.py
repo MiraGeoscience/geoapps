0459bac31fe9885a5e4e7f858ae38fd06d66f854
benk-mira <benjamink@mirageoscience.com>
2021-10-22 08:39:32 -0700

    Add gxz and gyz to list of sign flipped components in normalization

--- geoapps/drivers/components/data.py
+++ geoapps/drivers/components/data.py
@@ -417,7 +417,7 @@ class InversionData(InversionLocations):
         d = deepcopy(data)
         normalizations = {}
         for comp in self.components:
-            if comp in ["gz", "bz", "bxz", "byz"]:
+            if comp in ["gz", "bz", "gxz", "gyz", "bxz", "byz"]:
                 normalizations[comp] = -1.0
                 if d[comp] is not None:
                     d[comp] *= -1.0
