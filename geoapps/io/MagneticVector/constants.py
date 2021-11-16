a0b2b7880559d1f24f3c38c22fe91029832879fe
Dominique Fournier <dominiquef@mirageoscience.com>
2021-11-16 08:20:37 -0800

    Remove inf from choice of values

--- geoapps/io/MagneticVector/constants.py
+++ geoapps/io/MagneticVector/constants.py
@@ -815,7 +815,6 @@ app_initializer = {
     "octree_levels_topo": [0, 0, 0, 2],
     "octree_levels_obs": [5, 5, 5, 5],
     "depth_core": 500.0,
-    "max_distance": np.inf,
     "horizontal_padding": 1000.0,
     "vertical_padding": 1000.0,
     "window_center_x": 314600.0,
