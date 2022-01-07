12c4ae4037be96b4613cdf6a4e0ce9587de3f641
Dominique Fournier <dominiquef@mirageoscience.com>
2022-01-07 08:36:55 -0800

    Bump version.

--- geoapps/__init__.py
+++ geoapps/__init__.py
@@ -5,5 +5,5 @@
 #  geoapps is distributed under the terms and conditions of the MIT License
 #  (see LICENSE file at the root of this source code package).
 
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 # from .drivers import grav_inversion, mvi_inversion, pf_inversion
