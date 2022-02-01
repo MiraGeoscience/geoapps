64494b19e6f81aee6dbc5214cf7f00cf99174d9f
benjamink <benjamink@mirageoscience.com>
2022-02-01 13:19:23 -0800

    use saveiterationgeoh5 directive to save results after forward_only simulate call.

--- geoapps/drivers/components/factories/__init__.py
+++ geoapps/drivers/components/factories/__init__.py
@@ -5,7 +5,7 @@
 #  geoapps is distributed under the terms and conditions of the MIT License
 #  (see LICENSE file at the root of this source code package).
 
-from .directives_factory import DirectivesFactory
+from .directives_factory import DirectivesFactory, SaveIterationGeoh5Factory
 from .misfit_factory import MisfitFactory
 from .simulation_factory import SimulationFactory
 from .survey_factory import SurveyFactory, receiver_group
