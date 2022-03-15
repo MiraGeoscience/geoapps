231588008bc51f9dd059a67de2bdc9cb1a447b49
dominiquef <dominiquef@mirageoscience.com>
2022-03-15 11:56:43 -0700

    Specify defaults for optional reference model

--- geoapps/io/MagneticScalar/constants.py
+++ geoapps/io/MagneticScalar/constants.py
@@ -107,7 +107,7 @@ inversion_defaults = {
     "y_norm": 2.0,
     "z_norm": 2.0,
     "reference_model_object": None,
-    "reference_model": None,
+    "reference_model": 0.0,
     "gradient_type": "total",
     "lower_bound_object": None,
     "lower_bound": None,
