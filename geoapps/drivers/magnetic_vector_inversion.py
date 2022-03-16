ba44ed9276f481f229edc4e1261a646fd9249c7b
dominiquef <dominiquef@mirageoscience.com>
2022-03-16 14:14:17 -0700

    Add the rest of old inversion.py files. Update constants

---
+++ geoapps/drivers/magnetic_vector_inversion.py
@@ -0,0 +1,20 @@
+#  Copyright (c) 2022 Mira Geoscience Ltd.
+#
+#  This file is part of geoapps.
+#
+#  geoapps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+
+import sys
+import warnings
+
+from .magnetic_vector.inversion import start_inversion
+
+if __name__ == "__main__":
+    filepath = sys.argv[1]
+    warnings.warn(
+        "'geoapps.drivers.magnetic_vector_inversion' moved to "
+        "'geoapps.drivers.magnetic_vector.inversion' in version 0.2.0. "
+        "This warning is likely due to the execution of older ui.json files. Please update."
+    )
+    start_inversion(filepath)
