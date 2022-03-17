a3227cda8a34bc5dbb9f7400a088e64c06767321
benjamink <benjamink@mirageoscience.com>
2022-03-17 15:29:53 -0700

    Clean up the drivers

--- geoapps/drivers/grav_inversion.py
+++ geoapps/drivers/grav_inversion.py
@@ -5,7 +5,6 @@
 #  geoapps is distributed under the terms and conditions of the MIT License
 #  (see LICENSE file at the root of this source code package).
 
-
 import sys
 
 from geoh5py.ui_json import InputFile
