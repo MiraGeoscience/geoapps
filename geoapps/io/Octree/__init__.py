00244a7d67db7b120dd8e98b0ed6b23c936a5836
dominiquef <dominiquef@mirageoscience.com>
2022-03-17 13:06:29 -0700

    Remove unused FreeParams Validation

--- geoapps/io/Octree/__init__.py
+++ geoapps/io/Octree/__init__.py
@@ -5,7 +5,6 @@
 #  geoapps is distributed under the terms and conditions of the MIT License
 #  (see LICENSE file at the root of this source code package).
 
-from ..validation import InputFreeformValidation
 from .constants import (
     app_initializer,
     default_ui_json,
