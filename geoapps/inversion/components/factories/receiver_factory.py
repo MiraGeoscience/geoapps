c7b81e2a9a7e44b837a26ab1ccd58c2ef80eec14
benk-mira <81254271+benk-mira@users.noreply.github.com>
2022-03-31 12:19:45 -0700

    Update geoapps/inversion/components/factories/receiver_factory.py
    Co-authored-by: domfournier <dominiquef@mirageoscience.com>

--- geoapps/inversion/components/factories/receiver_factory.py
+++ geoapps/inversion/components/factories/receiver_factory.py
@@ -138,7 +138,7 @@ class ReceiversFactory(SimPEGFactory):
             draped_locs = drapeTopotoLoc(mesh, locs, actind=active_cells)
             # offset = np.linalg.norm(locs - draped_locs, axis=1)
             locs = draped_locs.copy()
-
+# TODO: Add drape object to ouput
             # if not self.params.geoh5.get_entity("draped locations"):
             #     parent = self.params.geoh5.get_entity(self.params.out_group)[0]
             #     drape = Points.create(
