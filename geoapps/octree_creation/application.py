bdf7656075fc212e1d0482380c055b4573f68442
dominiquef <dominiquef@mirageoscience.com>
2022-04-14 10:18:06 -0700

    Fix live_link output for octree and peakfinder.

--- geoapps/octree_creation/application.py
+++ geoapps/octree_creation/application.py
@@ -222,10 +222,15 @@ class OctreeMesh(ObjectDataSelection):
                 param_dict[key] = value.copy(parent=new_workspace, copy_children=True)
 
         param_dict["geoh5"] = new_workspace
+
+        if self.live_link.value:
+            param_dict["monitoring_directory"] = self.monitoring_directory
+
         ifile = InputFile(
             ui_json=self.params.input_file.ui_json,
             validation_options={"disabled": True},
         )
+
         new_params = OctreeParams(input_file=ifile, **param_dict)
         new_params.write_input_file()
         self.run(new_params)
