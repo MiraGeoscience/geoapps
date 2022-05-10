7d7afb202847ba4b22fb007508019380d2dbe268
jamiefb <jamieb@mirageoscience.com>
2022-05-10 13:27:29 -0700

    add params, constants, driver files to run iso surfaces from terminal

---
+++ geoapps/iso_surfaces/driver.py
@@ -0,0 +1,74 @@
+#  Copyright (c) 2022 Mira Geoscience Ltd.
+#
+#  This file is part of geoapps.
+#
+#  geoapps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+
+
+from __future__ import annotations
+
+import os
+
+import sys
+
+from geoh5py.groups import ContainerGroup
+from geoh5py.objects import Surface
+from geoh5py.ui_json import InputFile
+
+from geoh5py.ui_json.utils import monitored_directory_copy
+from geoapps.utils.utils import input_string_2_float
+from geoapps.utils.formatters import string_name
+from geoapps.iso_surfaces.utils import iso_surface
+
+from geoapps.iso_surfaces.params import IsoSurfacesParams
+
+
+class IsoSurfacesDriver:
+    def __init__(self, params: IsoSurfacesParams):
+        self.params: IsoSurfacesParams = params
+
+    def run(self):
+        """
+        Create iso surfaces from input values
+        """
+
+        levels = input_string_2_float(self.params.contours)
+
+        if levels is None:
+            return
+
+        surfaces = iso_surface(
+            self.params.objects,
+            self.params.data.values,
+            levels,
+            resolution=self.params.resolution,
+            max_distance=self.params.max_distance,
+        )
+
+        container = ContainerGroup.create(self.params.geoh5)
+        result = []
+        for ii, (surface, level) in enumerate(zip(surfaces, levels)):
+            if len(surface[0]) > 0 and len(surface[1]) > 0:
+                result += [
+                    Surface.create(
+                        self.params.geoh5,
+                        name=string_name(self.params.export_as + f"_{level:.2e}"),
+                        vertices=surface[0],
+                        cells=surface[1],
+                        parent=container,
+                    )
+                ]
+        if self.params.monitoring_directory is not None and os.path.exists(
+                self.params.monitoring_directory
+        ):
+            monitored_directory_copy(self.params.monitoring_directory, container)
+
+        return result
+
+
+if __name__ == "__main__":
+    file = sys.argv[1]
+    params = IsoSurfacesParams(InputFile.read_ui_json(file))
+    driver = IsoSurfacesDriver(params)
+    driver.run()
---
+++ geoapps/iso_surfaces/params.py
@@ -0,0 +1,114 @@
+#  Copyright (c) 2022 Mira Geoscience Ltd.
+#
+#  This file is part of geoapps.
+#
+#  geoapps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+
+from __future__ import annotations
+
+from copy import deepcopy
+
+from geoh5py.ui_json import InputFile
+from geoh5py.objects import ObjectBase
+from geoh5py.data import Data
+
+from geoapps.base.params import BaseParams
+
+from .constants import default_ui_json, defaults, validations
+
+
+class IsoSurfacesParams(BaseParams):
+    """
+    Parameter class for iso surfaces creation application.
+    """
+
+    def __init__(self, input_file=None, **kwargs):
+        self._default_ui_json = deepcopy(default_ui_json)
+        self._defaults = deepcopy(defaults)
+        self._validations = validations
+        self._objects = None
+        self._data = None
+        self._contours = None
+        self._max_distance = None
+        self._resolution = None
+        self._export_as = None
+
+        if input_file is None:
+            ui_json = deepcopy(self._default_ui_json)
+            input_file = InputFile(
+                ui_json=ui_json,
+                validations=self.validations,
+                validation_options={"disabled": True},
+            )
+
+        super().__init__(input_file=input_file, **kwargs)
+
+    @property
+    def objects(self) -> ObjectBase | None:
+        """
+        Input object
+        """
+        return self._objects
+
+    @objects.setter
+    def objects(self, val):
+        self.setter_validator("objects", val, fun=self._uuid_promoter)
+
+    @property
+    def data(self) -> Data | None:
+        """
+        Input data
+        """
+        return self._data
+
+    @data.setter
+    def data(self, val):
+        self.setter_validator("data", val)
+
+    @property
+    def contours(self) -> str | None:
+        """
+        String defining sets of contours.
+        Contours can be defined over an interval `50:200:10` and/or at a fix value `215`.
+        Any combination of the above can be used:
+        50:200:10, 215 => Contours between values 50 and 200 every 10, with a contour at 215.
+        """
+        return self._contours
+
+    @contours.setter
+    def contours(self, val):
+        self.setter_validator("contours", val)
+
+    @property
+    def max_distance(self) -> float | None:
+        """
+        Maximum distance from input data to generate iso surface.
+        """
+        return self._max_distance
+
+    @max_distance.setter
+    def max_distance(self, val):
+        self.setter_validator("max_distance", val)
+
+    @property
+    def resolution(self) -> float | None:
+        """
+        Grid size used to generate the iso surface.
+        """
+        return self._resolution
+
+    @resolution.setter
+    def resolution(self, val):
+        self.setter_validator("resolution", val)
+
+    @property
+    def export_as(self) -> str | None:
+        """
+        Name to save surface as.
+        """
+        return self._export_as
+
+    @export_as.setter
+    def export_as(self, val):
+        self.setter_validator("export_as", val)
