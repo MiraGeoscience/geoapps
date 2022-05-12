ea75c31e52b7bebedbabe2e83062c361d8799138
jamiefb <jamieb@mirageoscience.com>
2022-05-12 14:18:09 -0700

    randomize sphere properties

--- tests/iso_surfaces_test.py
+++ tests/iso_surfaces_test.py
@@ -11,18 +11,19 @@ import numpy as np
 import os
 from skimage.measure import marching_cubes
 from geoapps.iso_surfaces.driver import IsoSurfacesDriver
+import random
 
 def test_centroids():
     ws = Workspace(os.path.abspath("C:/Users/JamieB/Documents/GIT/geoapps/assets/iso_test.geoh5"))
 
     # Generate a 3D array
-    nx, ny, nz = 64, 64, 64 #(n-1)^3 points
+    nx, ny, nz = 70, 70, 70 #np.random.randint(25, 100, size=3) #70, 70, 70 #(n-1)^3 points
 
-    x = np.linspace(0, 10, nx)
-    y = np.linspace(0, 10, ny)
-    z = np.linspace(0, 10, nz)
+    x = np.linspace(0, random.randint(5, 15), nx)
+    y = np.linspace(0, random.randint(5, 15), ny)
+    z = np.linspace(0, random.randint(5, 15), nz)
 
-    origin = [0.0, 0.0, 0.0]
+    origin = np.random.randint(-100, 100, 3)
 
     # Create test block model
     block_model = BlockModel.create(
@@ -37,32 +38,22 @@ def test_centroids():
 
     # https://stackoverflow.com/questions/53326570/how-to-create-a-sphere-inside-an-ndarray/53339684#53339684
     # Sphere test data for the block model
-    size = (nx-1, ny-1, nz-1)
-    center = (15, 7, 25)
+    n = min([nx, ny, nz])
+    size = (n-1, n-1, n-1)#(nx-1, ny-1, nz-1)
+    center = ((nx-2)/2, (ny-2)/2, (nz-2)/2) + np.random.randint(-10, 10, 3)
     #center = ((nx-2)/2, (ny-2)/2, (nz-2)/2)
     distance = np.linalg.norm(np.subtract(np.indices(size).T, np.asarray(center)), axis=len(center))
-    sphere = np.ones(size) * (distance <= 6)
+    sphere = np.ones(size) * (distance <= random.randint(1, 20))
+    sphere = np.swapaxes(sphere, 1, 2) #.transpose((1, 2, 0))
 
     data = block_model.add_data(
         {
             "DataValues": {
                 "association": "CELL",
-                "values": sphere.flatten("F"), #C, F, A, K
+                "values": sphere.flatten("F"),
             }
         }
     )
-
-    '''
-    # Apply marching cubes
-    verts, faces, _, _ = marching_cubes(sphere, level=0)
-    
-    # Create test surface
-    surface = Surface.create(
-        ws,
-        vertices=verts,
-        cells=faces,
-        name="test_surface")
-    '''
 
     func_surface = IsoSurfacesDriver.iso_surface(block_model, sphere, [0], resolution=100.0, max_distance=np.inf)
 
@@ -72,8 +63,6 @@ def test_centroids():
         vertices=func_surface[0][0],
         cells=func_surface[0][1]
     )
-
-
 
 
 test_centroids()
