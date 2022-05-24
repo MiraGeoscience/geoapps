e41fac93715eaf28eccef781e8d6cc8e6862d733
benjamink <benjamink@mirageoscience.com>
2022-05-24 14:04:26 -0700

    Fix the existing_file_incrementer and update the FlinFlon geoh5 files to new format

--- geoapps/utils/string.py
+++ geoapps/utils/string.py
@@ -13,14 +13,20 @@ def existing_file_incrementer(filename):
     """Add an increment of the form (n) to the filename."""
 
     if os.path.exists(filename):
+
         root, file = filename.rsplit(os.path.sep, 1)
         name, extension = file.split(".", 1)
         incremented = re.search(r"\(([0-9]+)\)", name)
+
         if incremented is None:
             filename = os.path.join(root, f"{name} (1).{extension}")
+
         else:
             increment = incremented.group(0)
             name = name.replace(increment, f"({int(incremented.group(1)) + 1})")
             filename = os.path.join(root, f"{name}.{extension}")
+
+        if os.path.exists(filename):
+            filename = existing_file_incrementer(filename)
 
     return filename
