1b07aea75e77683d14700597162694d2b8e63050
sebastienh <sebastienh@mirageoscience.com>
2022-05-25 14:33:45 -0400

    fix install_or_update to rely on packages from the environment only

--- Install_or_Update.bat
+++ Install_or_Update.bat
@@ -9,10 +9,8 @@ if !errorlevel! neq 0 (
 set MY_CONDA=!MY_CONDA_EXE:"=!
 cd %~dp0
 call "!MY_CONDA!" remove --name geoapps --all --yes
-call "!MY_CONDA!" create --name geoapps --yes
+call "!MY_CONDA!" env create -f environment.yml
 call "!MY_CONDA!" activate geoapps
-call "!MY_CONDA!" install --yes --quiet matplotlib ipython h5py
-call "!MY_CONDA!" env update --file environment.yml --prune
 call python -m pip install -e . --no-deps
 pause
 cmd /k
