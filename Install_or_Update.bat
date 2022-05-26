10dbd948d28112a5814da33f25916f4d029f646d
sebastienh <sebastienh@mirageoscience.com>
2022-05-26 18:40:32 -0400

    provide the environment name
    not anymore in the yml file

--- Install_or_Update.bat
+++ Install_or_Update.bat
@@ -9,7 +9,7 @@ if !errorlevel! neq 0 (
 set MY_CONDA=!MY_CONDA_EXE:"=!
 cd %~dp0
 call "!MY_CONDA!" remove --name geoapps --all --yes
-call "!MY_CONDA!" env create -f conda-py-3.9-win-64.lock.yml
+call "!MY_CONDA!" env create -f conda-py-3.9-win-64.lock.yml -n geoapps
 call "!MY_CONDA!" activate geoapps && python -m pip install -e . --no-deps
 pause
 cmd /k
