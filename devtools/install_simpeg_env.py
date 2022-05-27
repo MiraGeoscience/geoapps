fbd80c75ac6d1a58875284742eed9b7930dda16f
sebastienh <sebastienh@mirageoscience.com>
2022-05-26 23:49:45 -0400

    fix script to generate env for simpeg

--- devtools/install_simpeg_env.py
+++ devtools/install_simpeg_env.py
@@ -6,25 +6,51 @@
 #
 #  geoapps is distributed under the terms and conditions of the MIT License
 #  (see LICENSE file at the root of this source code package).
+
 import os
 import subprocess
 from datetime import datetime
+from pathlib import Path
 
-timestamp = datetime.now().strftime("%G%m%d-%H%M%S")
-py_ver = "3.9"
-lock_file = f"conda-py-{py_ver}-lock.yml"
-env_name = f"geoapps-simpeg-{timestamp}"
+from run_conda_lock import per_platform_env
 
-print(f"# Install environment {env_name} from {lock_file}")
-subprocess.run(
-    (
-        f"conda-lock install --no-dev -n {env_name} {lock_file} ^"
-        f"&& conda activate {env_name} && python -m pip install . -v --no-deps ^"
-        "&& conda deactivate ^"
-        f"conda-pack -n {env_name} -o {env_name}.zip",
-    ),
-    env=dict(os.environ, PYTHONUTF8="1"),
-    shell=True,
-    check=True,
-    stderr=subprocess.STDOUT,
-)
+if __name__ == "__main__":
+    timestamp = datetime.now().strftime("%G%m%d-%H%M%S")
+    py_ver = "3.9"
+    env_name = f"geoapps-simpeg-{timestamp}"
+
+    suffix = f"-simpeg{timestamp}"
+    per_platform_env(py_ver, full=False, dev=False, suffix=suffix)
+    lock_file = Path(f"conda-py-{py_ver}-win-64{suffix}.lock.yml")
+    assert lock_file.is_file()
+
+    print(f"# Install environment {env_name} from {lock_file.name}")
+    try:
+        subprocess.run(
+            f"""conda env create -f {lock_file.name} -n {env_name} ^
+            && conda activate {env_name} && python -m pip install . -v --no-deps ^
+            && conda deactivate
+            """,
+            env=dict(os.environ, PYTHONUTF8="1"),
+            shell=True,
+            check=True,
+            stderr=subprocess.STDOUT,
+        )
+        subprocess.run(
+            f"conda-pack -n {env_name} -o {env_name}.zip",
+            shell=True,
+            check=True,
+            stderr=subprocess.STDOUT,
+        )
+        subprocess.run(
+            f"conda remove -n {env_name} --all -y",
+            shell=True,
+            check=True,
+            stderr=subprocess.STDOUT,
+        )
+        lock_file.unlink()
+    finally:
+        for p in ("osx-64", "linux-64"):
+            f = Path(f"conda-py-{py_ver}-{p}{suffix}.lock.yml")
+            if f.is_file():
+                f.unlink()
--- devtools/run_conda_lock.py
+++ devtools/run_conda_lock.py
@@ -22,16 +22,17 @@ def create_multi_platform_lock(py_ver: str):
     )
 
 
-def per_platform_env(py_ver: str, dev=False):
+def per_platform_env(py_ver: str, full=True, dev=False, suffix=""):
     print(
         f"# Create per platform Conda env files for Python {py_ver} ({'WITH' if dev else 'NO'} dev dependencies) "
     )
     dev_dep_option = "--dev-dependencies" if dev else "--no-dev-dependencies"
     dev_suffix = "-dev" if dev else ""
+    extras_option = "-extras full" if full else ""
     subprocess.run(
         (
-            f"conda-lock render {dev_dep_option} --extras full -k env"
-            f" --filename-template conda-py-{py_ver}-{{platform}}{dev_suffix}.lock conda-py-{py_ver}-lock.yml"
+            f"conda-lock render {dev_dep_option} {extras_option} -k env"
+            f" --filename-template conda-py-{py_ver}-{{platform}}{dev_suffix}{suffix}.lock conda-py-{py_ver}-lock.yml"
         ),
         env=dict(os.environ, PYTHONUTF8="1"),
         shell=True,
@@ -40,7 +41,8 @@ def per_platform_env(py_ver: str, dev=False):
     )
 
 
-for py_ver in ["3.9", "3.8", "3.7"]:
-    create_multi_platform_lock(py_ver)
-    per_platform_env(py_ver, dev=False)
-    per_platform_env(py_ver, dev=True)
+if __name__ == "__main__":
+    for py_ver in ["3.9", "3.8", "3.7"]:
+        create_multi_platform_lock(py_ver)
+        per_platform_env(py_ver, dev=False)
+        per_platform_env(py_ver, dev=True)
