a424b05bddfb100ff9dd92a92e68fc5777fda7e7
sebastienh <sebastienh@mirageoscience.com>
2022-05-28 15:26:25 -0400

    build simpeg env from prepared locked file
    no more need for conda-lock in github pipeline

--- devtools/install_simpeg_env.py
+++ devtools/install_simpeg_env.py
@@ -12,20 +12,17 @@ import subprocess
 from datetime import datetime
 from pathlib import Path
 
-from run_conda_lock import per_platform_env
-
 if __name__ == "__main__":
     timestamp = datetime.now().strftime("%G%m%d-%H%M%S")
-    py_ver = "3.9"
     env_name = f"geoapps-simpeg-{timestamp}"
 
-    suffix = f"-simpeg{timestamp}"
-    per_platform_env(py_ver, full=False, dev=False, suffix=suffix)
-    lock_file = Path(f"conda-py-{py_ver}-win-64{suffix}.lock.yml")
+    py_ver = "3.9"
+    platform = "win-64"
+    lock_file = Path(f"conda-py-{py_ver}-{platform}-simpeg.lock.yml")
     assert lock_file.is_file()
 
-    print(f"# Install environment {env_name} from {lock_file.name}")
     try:
+        print(f"# Create environment {env_name} from {lock_file.name}")
         subprocess.run(
             f"""conda env create -f {lock_file.name} -n {env_name} ^
             && conda activate {env_name} && python -m pip install . -v --no-deps ^
@@ -36,21 +33,17 @@ if __name__ == "__main__":
             check=True,
             stderr=subprocess.STDOUT,
         )
+        print(f"# Pack environment {env_name}")
         subprocess.run(
             f"conda-pack -n {env_name} -o {env_name}.zip",
             shell=True,
             check=True,
             stderr=subprocess.STDOUT,
         )
+    finally:
         subprocess.run(
             f"conda remove -n {env_name} --all -y",
             shell=True,
             check=True,
             stderr=subprocess.STDOUT,
         )
-        lock_file.unlink()
-    finally:
-        for p in ("osx-64", "linux-64"):
-            f = Path(f"conda-py-{py_ver}-{p}{suffix}.lock.yml")
-            if f.is_file():
-                f.unlink()
