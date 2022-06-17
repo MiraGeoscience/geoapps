510ddafe78e42803fd94deb5f4d1107667199ae2
benjamink <benjamink@mirageoscience.com>
2022-06-17 15:23:01 -0700

    use the timestamped temp file trick

--- geoapps/iso_surfaces/application.py
+++ geoapps/iso_surfaces/application.py
@@ -9,6 +9,7 @@ from __future__ import annotations
 
 import os
 import uuid
+from time import time
 
 from geoh5py.shared import Entity
 from geoh5py.ui_json import InputFile
@@ -101,8 +102,9 @@ class IsoSurface(ObjectDataSelection):
             except AttributeError:
                 continue
 
+        temp_geoh5 = f"{self.ga_group_name.value}_{time():.3f}.geoh5"
         new_workspace = self.get_output_workspace(
-            self.export_directory.selected_path, self.ga_group_name.value
+            self.export_directory.selected_path, temp_geoh5
         )
 
         param_dict["objects"] = param_dict["objects"].copy(
@@ -127,6 +129,7 @@ class IsoSurface(ObjectDataSelection):
 
         if self.live_link.value:
             print("Live link active. Check your ANALYST session for new mesh.")
+
 
     def data_change(self, _):
 
