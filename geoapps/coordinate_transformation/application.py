df4d0efb5453e54763445acbaf634db80bd1d19b
dominiquef <dominiquef@mirageoscience.com>
2022-07-12 10:45:39 -0700

    Move coord transforms import to top

--- geoapps/coordinate_transformation/application.py
+++ geoapps/coordinate_transformation/application.py
@@ -23,8 +23,6 @@ from geoapps.utils import warn_module_not_found
 from geoapps.utils.io import export_grid_2_geotiff
 from geoapps.utils.plotting import plot_plan_data_selection
 
-from .utils import geotiff_2_grid
-
 with warn_module_not_found():
     from ipywidgets import HBox, Layout, SelectMultiple, Text, Textarea, VBox
 
@@ -33,6 +31,8 @@ with warn_module_not_found():
 
 with warn_module_not_found():
     from osgeo import gdal, osr
+
+    from .utils import geotiff_2_grid
 
 
 class CoordinateTransformation(ObjectDataSelection):
