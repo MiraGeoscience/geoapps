7df42936f4359ae8e27a962f7fa807bf7752a5fe
dominiquef <dominiquef@mirageoscience.com>
2022-09-12 11:59:39 -0700

    Attempt to fix thread crash

--- tests/run_tests/__init__.py
+++ tests/run_tests/__init__.py
@@ -4,8 +4,13 @@
 #
 #  geoapps is distributed under the terms and conditions of the MIT License
 #  (see LICENSE file at the root of this source code package).
+import multiprocessing
 
 from dask.distributed import Client, LocalCluster
 
-cluster = LocalCluster(processes=False)
+cluster = LocalCluster(
+    processes=False,
+    n_workers=int(multiprocessing.cpu_count() / 2) - 1,
+    threads_per_worker=2,
+)
 Client(cluster)
