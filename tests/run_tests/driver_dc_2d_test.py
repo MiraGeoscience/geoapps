d2224e292c58e81993dc804368d43ca881fbef82
benjamink <benjamink@mirageoscience.com>
2022-09-27 10:19:47 -0700

    Fix up Dom's comments and update the 2d run test to conform with recent updates (split into two tests).

--- tests/run_tests/driver_dc_2d_test.py
+++ tests/run_tests/driver_dc_2d_test.py
@@ -8,8 +8,12 @@
 import os
 
 import numpy as np
+from geoh5py.workspace import Workspace
 
-from geoapps.inversion.driver import start_inversion
+from geoapps.inversion.driver import InversionDriver, start_inversion
+from geoapps.inversion.electricals.direct_current.two_dimensions.params import (
+    DirectCurrent2DParams,
+)
 from geoapps.shared_utils.utils import get_inversion_output
 from geoapps.utils.surveys import survey_lines
 from geoapps.utils.testing import check_target, setup_inversion_workspace
@@ -23,23 +27,18 @@ target_run = {
     "phi_m": 2.502,
 }
 
+np.random.seed(0)
 
-def test_dc_2d_run(
+
+def test_dc_2d_fwr_run(
     tmp_path,
     n_electrodes=10,
     n_lines=3,
-    max_iterations=1,
-    pytest=True,
     refinement=(4, 6),
 ):
-    from geoapps.inversion.driver import InversionDriver
-    from geoapps.inversion.electricals.direct_current.two_dimensions.params import (
-        DirectCurrent2DParams,
-    )
 
-    np.random.seed(0)
     # Run the forward
-    geoh5, mesh, model, survey, topography = setup_inversion_workspace(
+    geoh5, _, model, survey, topography = setup_inversion_workspace(
         tmp_path,
         background=0.01,
         anomaly=10,
@@ -66,57 +65,74 @@ def test_dc_2d_run(
     fwr_driver = InversionDriver(params)
     fwr_driver.run()
 
-    geoh5.open()
-    potential = geoh5.get_entity("Iteration_0_dc")[0]
-    _ = survey_lines(potential.parent, [-100, 100], save="line_IDs")
+    return fwr_driver.starting_model
 
-    # Run the inverse
-    np.random.seed(0)
-    params = DirectCurrent2DParams(
-        geoh5=geoh5,
-        mesh=mesh.uid,
-        topography_object=topography.uid,
-        data_object=potential.parent.uid,
-        potential_channel=potential.uid,
-        potential_uncertainty=1e-3,
-        line_object=geoh5.get_entity("line_IDs")[0].uid,
-        line_id=2,
-        starting_model=1e-2,
-        s_norm=0.0,
-        x_norm=1.0,
-        y_norm=1.0,
-        z_norm=1.0,
-        gradient_type="components",
-        potential_channel_bool=True,
-        z_from_topo=True,
-        max_iterations=max_iterations,
-        initial_beta=None,
-        initial_beta_ratio=1e0,
-        prctile=100,
-        upper_bound=10,
-    )
-    params.write_input_file(path=tmp_path, name="Inv_run")
+
+def test_dc_2d_run(tmp_path, max_iterations=1, pytest=True):
+
+    workpath = os.path.join(tmp_path, "inversion_test.geoh5")
+    if pytest:
+        workpath = str(tmp_path / "../test_dc_2d_fwr_run0/inversion_test.geoh5")
+
+    with Workspace(workpath) as geoh5:
+        potential = geoh5.get_entity("Iteration_0_dc")[0]
+        mesh = geoh5.get_entity("Models")[0]
+        topography = geoh5.get_entity("topography")[0]
+        _ = survey_lines(potential.parent, [-100, 100], save="line_IDs")
+
+        # Run the inverse
+        np.random.seed(0)
+        params = DirectCurrent2DParams(
+            geoh5=geoh5,
+            mesh=mesh.uid,
+            topography_object=topography.uid,
+            data_object=potential.parent.uid,
+            potential_channel=potential.uid,
+            potential_uncertainty=1e-3,
+            line_object=geoh5.get_entity("line_IDs")[0].uid,
+            line_id=2,
+            starting_model=1e-2,
+            s_norm=0.0,
+            x_norm=1.0,
+            y_norm=1.0,
+            z_norm=1.0,
+            gradient_type="components",
+            potential_channel_bool=True,
+            z_from_topo=True,
+            max_iterations=max_iterations,
+            initial_beta=None,
+            initial_beta_ratio=1e0,
+            prctile=100,
+            upper_bound=10,
+        )
+        params.write_input_file(path=tmp_path, name="Inv_run")
+
     driver = start_inversion(os.path.join(tmp_path, "Inv_run.ui.json"))
 
     output = get_inversion_output(
         driver.params.geoh5.h5file, driver.params.ga_group.uid
     )
-    output["data"] = potential.values[np.isfinite(potential.values)]
+    if geoh5.open():
+        output["data"] = potential.values[np.isfinite(potential.values)]
     if pytest:
         check_target(output, target_run)
     else:
-        return fwr_driver.starting_model, driver.inverse_problem.model
+        return driver.inverse_problem.model
 
 
 if __name__ == "__main__":
     # Full run
-    m_start, m_rec = test_dc_2d_run(
+    m_start = test_dc_2d_fwr_run(
         "./",
         n_electrodes=20,
         n_lines=3,
+        refinement=(4, 8),
+    )
+
+    m_rec = test_dc_2d_run(
+        "./",
         max_iterations=20,
         pytest=False,
-        refinement=(4, 8),
     )
     residual = np.linalg.norm(m_rec - m_start) / np.linalg.norm(m_start) * 100.0
     assert (
