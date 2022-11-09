cb495c8635f74ebcacd4aed47886dbe31e8dd33b
benjamink <benjamink@mirageoscience.com>
2022-11-09 15:06:21 -0800

    Cherry picked updates to pseudo3d from GEOPY-678

---
+++ geoapps/inversion/electricals/direct_current/pseudo_three_dimensions/driver.py
@@ -0,0 +1,20 @@
+#  Copyright (c) 2022 Mira Geoscience Ltd.
+#
+#  This file is part of geoapps.
+#
+#  geoapps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+
+from geoapps.inversion.line_sweep.driver import LineSweepDriver
+
+from .constants import validations
+from .params import DirectCurrentPseudo3DParams
+
+
+class DirectCurrentPseudo3DDriver(LineSweepDriver):
+
+    _params_class = DirectCurrentPseudo3DParams
+    _validations = validations
+
+    def __init__(self, params: DirectCurrentPseudo3DParams):  # pylint: disable=W0235
+        super().__init__(params)
