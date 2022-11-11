f30f6fa988cbaf518c7fa75b5f9c6a172b4e34cf
sebastienh <sebastienh@mirageoscience.com>
2022-11-11 15:29:02 -0500

    develop branch is targeting version 0.10.0
    set version to 0.10.0 alpha

--- geoapps/__init__.py
+++ geoapps/__init__.py
@@ -5,4 +5,4 @@
 #  geoapps is distributed under the terms and conditions of the MIT License
 #  (see LICENSE file at the root of this source code package).
 
-__version__ = "0.9.0-rc.4"
+__version__ = "0.10.0-alpha.1"
