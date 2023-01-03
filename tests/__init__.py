9ee9b957d35f0940bb957b84d5825ea92f92bac1
benjamink <benjamink@mirageoscience.com>
2023-01-03 10:13:24 -0800

    set flinflon dls to issue branch

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2022 Mira Geoscience Ltd.
+#  Copyright (c) 2023 Mira Geoscience Ltd.
 #
 #  This file is part of geoapps.
 #
@@ -11,9 +11,7 @@ import requests
 
 PROJECT = "FlinFlon.geoh5"
 
-URL = (
-    "https://github.com/MiraGeoscience/geoapps/raw/release/0.9.1/assets/FlinFlon.geoh5"
-)
+URL = "https://github.com/MiraGeoscience/geoapps/raw/GEOPY-621/assets/FlinFlon.geoh5"
 # Todo - change this back to release branch
 if not os.path.isfile("FlinFlon.geoh5"):
     r = requests.get(URL, timeout=5)
@@ -21,7 +19,9 @@ if not os.path.isfile("FlinFlon.geoh5"):
         file.write(r.content)
 
 PROJECT_DCIP = "FlinFlon_dcip.geoh5"
-URL = "https://github.com/MiraGeoscience/geoapps/raw/release/0.9.1/assets/FlinFlon_dcip.geoh5"
+URL = (
+    "https://github.com/MiraGeoscience/geoapps/raw/GEOPY-621/assets/FlinFlon_dcip.geoh5"
+)
 
 if not os.path.isfile("FlinFlon_dcip.geoh5"):
     r = requests.get(URL, timeout=5)
