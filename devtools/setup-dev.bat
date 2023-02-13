ec5df529e3b7d7fe20c221bcba59be44c7fe7b48
sebhmg <sebastienh@mirageoscience.com>
2023-02-13 13:55:47 -0500

    expose error conda activate fails when setting up the dev environment

--- devtools/setup-dev.bat
+++ devtools/setup-dev.bat
@@ -24,6 +24,12 @@ set env_path=%project_dir%\.conda-env
 call !MY_CONDA_EXE! activate
 call mamba env update -p %env_path% -f %project_dir%\environments\conda-py-%PY_VER%-win-64-dev.lock.yml
 call conda activate %env_path%
+
+if !errorlevel! neq 0 (
+  pause
+  exit /B !errorlevel!
+)
+
 if exist %project_dir%\..\geoh5py\ (
   pip install --upgrade --force-reinstall -e %project_dir%\..\geoh5py --no-deps
 )
