9d92d78c8207d1187179c4e948943212f8a6015c
sebhmg <sebastienh@mirageoscience.com>
2023-02-21 02:17:24 -0500

    recommend miniforge instead of mambaforge
    do not try to use mamba for installation

--- Install_or_Update.bat
+++ Install_or_Update.bat
@@ -13,18 +13,9 @@ set ENV_NAME=geoapps
 set MY_CONDA=!MY_CONDA_EXE:"=!
 cd %~dp0
 set PYTHONUTF8=1
-call "!MY_CONDA!" activate
-
-IF "%MY_CONDA:mamba.bat=%mamba.bat"=="%MY_CONDA%" (
-  set pkg_mgr_exe=mamba
-  echo "use mamba"
-) ELSE (
-  set pkg_mgr_exe=conda
-  echo "use conda"
-)
-
-call !pkg_mgr_exe! env create --force -n %ENV_NAME% --file environments\conda-py-%PY_VER%-win-64.lock.yml ^
-  && call !pkg_mgr_exe! run -n %ENV_NAME% pip install -e . --no-deps
+call "!MY_CONDA!" activate base ^
+  && call conda env create --force -n %ENV_NAME% --file environments\conda-py-%PY_VER%-win-64.lock.yml ^
+  && call conda run -n %ENV_NAME% pip install -e . --no-deps
 
 if !errorlevel! neq 0 (
   echo "** ERROR: Installation failed **"
@@ -33,4 +24,4 @@ if !errorlevel! neq 0 (
 )
 
 pause
-cmd /k "!pkg_mgr_exe! activate %ENV_NAME%"
+cmd /k "conda activate %ENV_NAME%"
