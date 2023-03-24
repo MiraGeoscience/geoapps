c4eceb112bd0f6a1d7f69711150a31c7a5e2c6d1
sebhmg <sebastienh@mirageoscience.com>
2023-03-24 00:22:27 -0400

    install assets under geoapps-assets

--- geoapps/__init__.py
+++ geoapps/__init__.py
@@ -6,3 +6,17 @@
 #  (see LICENSE file at the root of this source code package).
 
 __version__ = "0.10.0-alpha.1"
+
+from pathlib import Path
+
+
+def assets_path() -> Path:
+    """Return the path to the assets folder."""
+
+    parent = Path(__file__).parent
+    folder_name = f"{parent.name}-assets"
+    assets_folder = parent.parent / folder_name
+    if not assets_folder.is_dir():
+        raise RuntimeError(f"Assets folder not found: {assets_folder}")
+
+    return assets_folder
