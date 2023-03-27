50b8650e238e6929111ab76d5078bca99563030c
sebhmg <sebastienh@mirageoscience.com>
2023-03-27 12:33:05 -0400

    custom location for geoapps assets dir
    configure with environment variable GEOAPPS_ASSETS_DIR

--- geoapps/__init__.py
+++ geoapps/__init__.py
@@ -7,11 +7,24 @@
 
 __version__ = "0.10.0-alpha.1"
 
+import os
+import warnings
 from pathlib import Path
 
 
 def assets_path() -> Path:
     """Return the path to the assets folder."""
+
+    assets_dir_env_var = "GEOAPPS_ASSETS_DIR"
+    assets_dirname = os.environ.get(assets_dir_env_var, None)
+    if assets_dirname:
+        assets_folder = Path(assets_dirname)
+        if not assets_folder.is_dir():
+            warnings.warn(
+                f"Custom assets folder not found: {assets_dir_env_var}={assets_dirname}"
+            )
+        else:
+            return assets_folder
 
     parent = Path(__file__).parent
     folder_name = f"{parent.name}-assets"
--- tests/assets_path_test.py
+++ tests/assets_path_test.py
@@ -5,11 +5,31 @@
 #  geoapps is distributed under the terms and conditions of the MIT License
 #  (see LICENSE file at the root of this source code package).
 
+from __future__ import annotations
+
+from pathlib import Path
+
 from geoapps import assets_path
 
 
 def test_assets_directory_exist():
     assert assets_path().is_dir()
+
+
+def test_assets_directory_from_env(tmp_path: Path, monkeypatch):
+    monkeypatch.setenv("GEOAPPS_ASSETS_DIR", str(tmp_path.absolute()))
+
+    assert assets_path().is_dir()
+    assert tmp_path == assets_path()
+
+
+def test_assets_directory_from_wrong_env(tmp_path: Path, monkeypatch):
+    non_existing_path = tmp_path / "wrong"
+    monkeypatch.setenv("GEOAPPS_ASSETS_DIR", str(non_existing_path.absolute()))
+
+    assert non_existing_path.is_dir() is False
+    assert assets_path().is_dir() is True
+    assert non_existing_path != assets_path()
 
 
 def test_uijson_files_exists():
