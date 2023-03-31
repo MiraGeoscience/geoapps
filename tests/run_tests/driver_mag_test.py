dba535e49bce90f7148aedbd9611006a45671c41
dominiquef <dominiquef@mirageoscience.com>
2023-03-31 14:03:38 -0700

    Change ference to private _demote method. Update input_file.data on params.update

--- tests/run_tests/driver_mag_test.py
+++ tests/run_tests/driver_mag_test.py
@@ -5,9 +5,11 @@
 #  geoapps is distributed under the terms and conditions of the MIT License
 #  (see LICENSE file at the root of this source code package).
 
+import json
 import os
 
 import numpy as np
+from geoh5py.shared.utils import str2uuid
 from geoh5py.workspace import Workspace
 
 from geoapps.inversion.potential_fields import MagneticScalarParams
@@ -65,6 +67,20 @@ def test_susceptibility_fwr_run(
 
     fwr_driver.run()
 
+    with geoh5.open():
+        # Check the inversion output
+        sp_group = geoh5.get_entity("MagneticScalarForward")[0]
+
+        with open(params.input_file.path_name, encoding="utf-8") as file:
+            ui_json = json.load(file)
+
+        for key, values in sp_group.options.items():
+            if isinstance(values, dict):
+                for elem, value in values.items():
+                    assert str2uuid(ui_json[key][elem]) == value
+            else:
+                assert ui_json[key] == values
+
     return fwr_driver.starting_model
 
 
@@ -114,6 +130,7 @@ def test_susceptibility_run(
             store_sensitivities="ram",
         )
         params.write_input_file(path=tmp_path, name="Inv_run")
+
         driver = MagneticScalarDriver.start(os.path.join(tmp_path, "Inv_run.ui.json"))
 
     with Workspace(driver.params.geoh5.h5file) as run_ws:
