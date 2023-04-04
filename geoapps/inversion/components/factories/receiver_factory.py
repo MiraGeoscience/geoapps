57e56c0918465d5456ece6c802953727ac783c80
dominiquef <dominiquef@mirageoscience.com>
2023-04-04 16:29:53 -0700

    Patch conversion on receivers

--- geoapps/inversion/components/factories/receiver_factory.py
+++ geoapps/inversion/components/factories/receiver_factory.py
@@ -191,7 +191,15 @@ class ReceiversFactory(SimPEGFactory):
         return args
 
     def _tdem_arguments(self, data=None, locations=None, local_index=None, mesh=None):
-        return [locations, data.entity.channels]
+        conversion = {
+            "Seconds (s)": 1.0,
+            "Milliseconds (ms)": 1e-3,
+            "Microseconds (us)": 1e-6,
+        }
+        return [
+            locations,
+            np.asarray(data.entity.channels) * conversion[data.entity.unit],
+        ]
 
     def _magnetotellurics_arguments(self, locations=None, local_index=None, mesh=None):
         args = []
