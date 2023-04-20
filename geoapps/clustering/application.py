13baa4a15f19afcc7a23ded823debee427c44046
benjamink <benjamink@mirageoscience.com>
2023-04-20 14:40:42 -0700

    Update write_ui_json calls with validation_options

--- geoapps/clustering/application.py
+++ geoapps/clustering/application.py
@@ -1310,7 +1310,7 @@ class Clustering(ScatterPlots):
                 new_params.write_input_file(
                     name=temp_geoh5.replace(".geoh5", ".ui.json"),
                     path=monitoring_directory,
-                    validate=False,
+                    validation_options={"disabled": True},
                 )
                 # Run driver.
                 self.driver.params = new_params
