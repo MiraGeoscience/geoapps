b0819e72c9adb2e080d7944fd884ee2d12e3554b
Andrew Gray <andrewg@mirageoscience.com>
2023-05-12 13:19:23 -0700

    GEOPY-879 set version to 0.10.0

--- docs/conf.py
+++ docs/conf.py
@@ -92,12 +92,12 @@ source_suffix = ".rst"
 master_doc = "index"
 
 # General information about the project.
-project = "geoapps v0.10.0-rc.5"
+project = "geoapps v0.10.0"
 
 # The short X.Y version.
 version = "0.10.0"
 # The full version, including alpha/beta/rc tags.
-release = "0.10.0-rc.5"
+release = "0.10.0"
 
 
 # List of patterns, relative to source directory, that match files and
