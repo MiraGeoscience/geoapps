cb2618847c06601ee33b4006b56a743a8f34d3c6
sebhmg <sebastienh@mirageoscience.com>
2023-05-23 22:54:27 -0400

    point to update geoh5py and update env locks

--- devtools/run_conda_lock.bat
+++ devtools/run_conda_lock.bat
@@ -17,16 +17,7 @@ if !errorlevel! neq 0 (
   exit /B !errorlevel!
 )
 
-:: use test-pypi to get development versions if needed
-:: (Set it through the config command as simply setting
-::  POETRY_REPOSITORIES_TEST_PYPI_URL seems to have no effect
-::  when running conda-lock, despite it appears in poetry config --list)
-poetry config repositories.test_pypi https://test.pypi.org/simple/
-
 call !MY_CONDA_EXE! activate && python devtools\run_conda_lock.py
-
-:: do not keep test-pypi repository in the config
-poetry config --unset repositories.test_pypi
 
 pause
 cmd /k
