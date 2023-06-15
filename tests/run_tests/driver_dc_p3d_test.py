861bdd8e703ccd021c68a9132517929b15efe382
dominiquef <dominiquef@mirageoscience.com>
2023-06-15 11:54:53 -0700

    Fix path from comment

--- tests/run_tests/driver_dc_p3d_test.py
+++ tests/run_tests/driver_dc_p3d_test.py
@@ -88,7 +88,7 @@ def test_dc_p3d_run(
     max_iterations=1,
     pytest=True,
 ):
-    workpath = tmp_path / "inversion_test.geoh5"
+    workpath = tmp_path / "inversion_test.ui.geoh5"
     if pytest:
         workpath = tmp_path.parent / "test_dc_p3d_fwr_run0" / "inversion_test.ui.geoh5"
 
