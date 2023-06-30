fix data test and restore 1/normalization for general em.

geoapps/inversion/components/data.py
@@ -426,7 +426,7 @@ class InversionData(InversionLocations):
         for chan in getattr(self.params.data_object, "channels", [None]):
             normalizations[chan] = {}
             for comp in self.components:
-                normalizations[chan][comp] = 1
+                normalizations[chan][comp] = np.ones(self.mask.sum())
                 if comp in ["potential", "chargeability"]:
                     normalizations[chan][comp] = 1
                 if comp in ["gz", "bz", "gxz", "gyz", "bxz", "byz"]:

geoapps/inversion/components/factories/directives_factory.py
@@ -546,7 +546,7 @@ class SaveIterationGeoh5Factory(SimPEGFactory):
             "association": "VERTEX",
             "transforms": np.hstack(
                 [
-                    inversion_object.normalizations[chan][comp]
+                    1 / inversion_object.normalizations[chan][comp]
                     for chan in channels
                     for comp in components
                 ]