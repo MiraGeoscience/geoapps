d45e2a082fc89801ec9e678267339eca6805e382
dominiquef <dominiquef@mirageoscience.com>
2023-07-25 14:02:54 -0700

    Always store projections for non-potential fields

--- geoapps/inversion/components/factories/receiver_factory.py
+++ geoapps/inversion/components/factories/receiver_factory.py
@@ -129,21 +129,17 @@ class ReceiversFactory(SimPEGFactory):
         kwargs = {}
         if self.factory_type in ["gravity", "magnetic scalar", "magnetic vector"]:
             kwargs["components"] = list(data)
+        else:
+            kwargs["storeProjections"] = True
+
         if self.factory_type in ["fem", "magnetotellurics", "tipper"]:
             comp = component.split("_")[0]
             kwargs["orientation"] = comp[0] if self.factory_type == "fem" else comp[1:]
             kwargs["component"] = component.split("_")[1]
-            kwargs["storeProjections"] = True
         if self.factory_type in ["tipper"]:
             kwargs["orientation"] = kwargs["orientation"][::-1]
         if self.factory_type in ["tdem"]:
             kwargs["orientation"] = component
-            kwargs["storeProjections"] = True
-        if (
-            "direct current" in self.factory_type
-            or "induced polarization" in self.factory_type
-        ):
-            kwargs["storeProjections"] = True
 
         return kwargs
 
