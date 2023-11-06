c750be77dcfc6dd288291eb78fb5feac2b9372f3
dominiquef <dominiquef@mirageoscience.com>
2023-11-06 10:29:28 -0800

    Remove duplication in copyright header

--- geoapps/inversion/components/factories/entity_factory.py
+++ geoapps/inversion/components/factories/entity_factory.py
@@ -5,15 +5,6 @@
 #  geoapps is distributed under the terms and conditions of the MIT License
 #  (see LICENSE file at the root of this source code package).
 #
-#  This file is part of geoapps.
-#
-#  geoapps is distributed under the terms and conditions of the MIT License
-#  (see LICENSE file at the root of this source code package).
-#
-#  This file is part of geoapps.
-#
-#  geoapps is distributed under the terms and conditions of the MIT License
-#  (see LICENSE file at the root of this source code package).
 
 # pylint: disable=W0221
 
