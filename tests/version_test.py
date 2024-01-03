97fd591e49dd7244c1c44a690cb4810a7d345140
dominiquef <dominiquef@mirageoscience.com>
2024-01-03 14:55:20 -0800

    Two more files

--- tests/version_test.py
+++ tests/version_test.py
@@ -4,10 +4,7 @@
 #
 #  geoapps is distributed under the terms and conditions of the MIT License
 #  (see LICENSE file at the root of this source code package).
-#
-#  This file is part of my_app project.
-#
-#  All rights reserved.
+
 
 from __future__ import annotations
 
