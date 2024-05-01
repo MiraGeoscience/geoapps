a36f7adb9859e0358ce4177fb13815ffe4c4ab19
Sébastien Hensgen <sebastienh@mirageoscience.com>
2024-05-01 16:34:15 -0400

    GEOPY-1472: fix flake8

--- geoapps/inversion/driver.py
+++ geoapps/inversion/driver.py
@@ -428,7 +428,7 @@ class InversionDriver(BaseDriver):
         inversion_type = ifile.data["inversion_type"]
         if inversion_type not in DRIVER_MAP:
             msg = f"Inversion type {inversion_type} is not supported."
-            msg += f" Valid inversions are: {*list(DRIVER_MAP),}."
+            msg += f" Valid inversions are: {*list(DRIVER_MAP), }."
             raise NotImplementedError(msg)
 
         mod_name, class_name = DRIVER_MAP.get(inversion_type)
