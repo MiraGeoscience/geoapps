9cedad1c4cc6be01c685a372e7cdb7f8806da397
dominiquef <dominiquef@mirageoscience.com>
2025-02-18 13:38:27 -0800

    Fix start notebook

--- geoapps/scripts/start_notebook.py
+++ geoapps/scripts/start_notebook.py
@@ -13,7 +13,7 @@ import importlib.util
 import logging
 from pathlib import Path
 
-from notebook.notebookapp import main as notebook_main
+from notebook.app import main as notebook_main
 
 
 def main():
@@ -25,7 +25,6 @@ def main():
         logging.getLogger(__package__).error(
             "Could not find index.ipynb (looking in %s)", geoapps_root.resolve()
         )
-
     notebook_main([str(index_notebook.resolve())])
 
 
